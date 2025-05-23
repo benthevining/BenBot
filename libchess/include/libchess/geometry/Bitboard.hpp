0390bd2e58e90b7f95b21e69ce8dbd80638a193f
Ben Vining <ben.the.vining@gmail.com>
2025-05-22 22:31:19 -0500

    feat: initial commit of Square class (#2)

--- libchess/include/libchess/geometry/Bitboard.hpp
+++ libchess/include/libchess/geometry/Bitboard.hpp
@@ -9,8 +9,10 @@
 #pragma once
 
 #include <bitset>
-#include <cstddef> // IWYU pragma: keep - for size_t
+#include <cassert>
+#include <cstddef>
 #include <cstdint>
+#include <libchess/geometry/Square.hpp>
 
 namespace chess {
 
@@ -19,27 +21,24 @@ namespace chess {
 
     Each bit represents a square of the chessboard; the bit is 1 if there is a piece there,
     and 0 if the square is empty.
-
-    @todo test/set specific squares
  */
 struct Bitboard final {
-    /** Integer type used for serialization of bitboards. */
+    /** Unsigned integer type used for serialization of bitboards. */
     using Integer = std::uint_least64_t;
 
     /** Constructs an empty bitboard. */
     constexpr Bitboard() noexcept = default;
 
-    /** Constructs a bitboard from an integer representation. */
+    /** Constructs a bitboard from an integer representation.
+        @see to_int()
+     */
     explicit constexpr Bitboard(Integer value) noexcept
         : bits { static_cast<unsigned long long>(value) } // NOLINT(runtime/int)
     {
     }
 
     /** Returns true if the two bitboards have all the same bits set. */
-    [[nodiscard]] constexpr bool operator==(const Bitboard& other) const noexcept
-    {
-        return bits == other.bits;
-    }
+    [[nodiscard]] constexpr bool operator==(const Bitboard& other) const noexcept { return bits == other.bits; }
 
     /** Returns true if any of the bits are set. */
     [[nodiscard]] constexpr bool any() const noexcept { return bits.any(); }
@@ -50,14 +49,35 @@ struct Bitboard final {
     /** Returns the number of bits that are set. */
     [[nodiscard]] constexpr std::size_t count() const noexcept { return bits.count(); }
 
+    /** Returns true if there is a piece on the given square. */
+    [[nodiscard]] constexpr bool test(Square square) const noexcept { return test(square.index()); }
+
+    /** Returns true if there is a piece on the given square.
+        This method asserts if the given index is greater than 63.
+     */
+    [[nodiscard]] constexpr bool test(BitboardIndex index) const noexcept
+    {
+        assert(index <= 63uz);
+        return bits[index];
+    }
+
+    /** Sets the given square's bit to the given value. */
+    constexpr void set(Square square, bool value = true) noexcept { set(square.index(), value); }
+
+    /** Sets the given square's bit to the given value.
+        This method asserts if the given index is greater than 63.
+     */
+    constexpr void set(BitboardIndex index, bool value = true) noexcept
+    {
+        assert(index <= 63uz);
+        bits[index] = value;
+    }
+
     /** Resets all bits to 0. */
-    constexpr void reset() noexcept { bits.reset(); }
+    constexpr void clear() noexcept { bits.reset(); }
 
     /** Converts this bitboard to its integer representation. */
-    [[nodiscard]] constexpr Integer to_int() const noexcept
-    {
-        return static_cast<Integer>(bits.to_ullong());
-    }
+    [[nodiscard]] constexpr Integer to_int() const noexcept { return static_cast<Integer>(bits.to_ullong()); }
 
 private:
     std::bitset<64uz> bits;
--- libchess/include/libchess/geometry/Square.hpp
+++ libchess/include/libchess/geometry/Square.hpp
@@ -7,3 +7,124 @@
  */
 
 #pragma once
+
+#include <cassert>
+#include <cstdint>
+#include <utility>
+
+namespace chess {
+
+/** Unsigned integer type used for bitboard indices. */
+using BitboardIndex = std::uint_fast64_t;
+
+/** This enum describes the ranks of the chessboard.
+    @see File
+ */
+enum class Rank : BitboardIndex {
+    One,   ///< The first rank. This is the rank that white's king starts on.
+    Two,   ///< The second rank. This is the rank that white's pawns start on.
+    Three, ///< The third rank.
+    Four,  ///< The fourth rank.
+    Five,  ///< The fifth rank.
+    Six,   ///< The sixth rank.
+    Seven, ///< The seventh rank. This is the rank that black's pawns start on.
+    Eight  ///< The back rank. This is the rank that black's king starts on.
+};
+
+/** This enum describes the files of the chess board.
+    @see Rank
+ */
+enum class File : BitboardIndex {
+    A, ///< The A file.
+    B, ///< The B file.
+    C, ///< The C file.
+    D, ///< The D file. This is the file that the queens start on.
+    E, ///< The E file. This is the file that the kings start on.
+    F, ///< The F file.
+    G, ///< The G file.
+    H  ///< The H file.
+};
+
+/** This struct uniquely identifies a square on the chessboard via its rank and file,
+    and provides mappings to and from bitboard indices.
+
+    This class uses the "Least Significant File" mapping to calculate bitboard indices,
+    as opposed to the "Least Significant Rank" mapping. This means that ranks are aligned
+    to the eight consecutive bytes of a bitboard.
+
+    This results in the following mapping of squares to bitboard indices:
+    @verbatim
+          A |  B |  C |  D |  E |  F |  G |  H |
+    8  | 56 | 57 | 58 | 59 | 60 | 61 | 62 | 63 |
+    7  | 48 | 49 | 50 | 51 | 52 | 53 | 54 | 55 |
+    6  | 40 | 41 | 42 | 43 | 44 | 45 | 46 | 47 |
+    5  | 32 | 33 | 34 | 35 | 36 | 37 | 38 | 39 |
+    4  | 24 | 25 | 26 | 27 | 28 | 29 | 30 | 31 |
+    3  | 16 | 17 | 18 | 19 | 20 | 21 | 22 | 23 |
+    2  | 8  | 9  | 10 | 11 | 12 | 13 | 14 | 15 |
+    1  | 0  | 1  | 2  | 3  | 4  | 5  | 6  | 7  |
+    @endverbatim
+
+    @todo is_dark()/is_light()
+    @todo std::formatter
+    @todo from_string()
+    @todo free functions to iterate ranks, files, diagonals
+ */
+struct Square final {
+    /** This square's rank. */
+    Rank rank { Rank::One };
+
+    /** This square's file. */
+    File file { File::A };
+
+    /** Returns the bitboard bit index for this square.
+        The returned index will be in the range ``[0,63]``.
+     */
+    [[nodiscard]] constexpr BitboardIndex index() const noexcept
+    {
+        return static_cast<BitboardIndex>(8) * static_cast<BitboardIndex>(rank) + static_cast<BitboardIndex>(file);
+    }
+
+    /** Calculates the rank and file corresponding to the given bitboard index.
+        This function asserts if the passed ``index`` is greater than 63.
+     */
+    [[nodiscard]] static constexpr Square from_index(BitboardIndex index) noexcept
+    {
+        assert(std::cmp_less_equal(index, 63uz));
+
+        return {
+            .rank = static_cast<Rank>(index >> static_cast<BitboardIndex>(3)),
+            .file = static_cast<File>(index & static_cast<BitboardIndex>(7))
+        };
+    }
+
+    /** Returns true if this square is on the queenside (the A-D files). */
+    [[nodiscard]] constexpr bool is_queenside() const noexcept
+    {
+        return std::cmp_less_equal(
+            std::to_underlying(file), std::to_underlying(File::D));
+    }
+
+    /** Returns true if this square is on the kingside (the E-H files). */
+    [[nodiscard]] constexpr bool is_kingside() const noexcept
+    {
+        return std::cmp_greater_equal(
+            std::to_underlying(file), std::to_underlying(File::E));
+    }
+
+    /** Returns true if this square is within White's territory (the first through fourth ranks). */
+    [[nodiscard]] constexpr bool is_white_territory() const noexcept
+    {
+        return std::cmp_less_equal(
+            std::to_underlying(rank), std::to_underlying(Rank::Four));
+    }
+
+    /** Returns true if this square is within Black's territory (the fifth through eighth ranks). */
+    [[nodiscard]] constexpr bool is_black_territory() const noexcept
+    {
+        return std::cmp_greater_equal(
+            std::to_underlying(rank), std::to_underlying(Rank::Five));
+    }
+};
+
+} // namespace chess
