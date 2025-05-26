f832b99ecd62e717365ac97fa88a61cf5516881f
Ben Vining <ben.the.vining@gmail.com>
2025-05-26 00:15:43 -0500

    test: printer executable

---
+++ tests/printer/printer.cpp
@@ -0,0 +1,27 @@
+/*
+ * ======================================================================================
+ *
+ * libchess - a chess engine by Ben Vining
+ *
+ * ======================================================================================
+ */
+
+#include <libchess/game/Position.hpp>
+#include <print>
+
+int main()
+{
+    using chess::game::print_utf8;
+
+    chess::game::Position position;
+
+    std::println("{}", print_utf8(position));
+
+    std::println("");
+
+    position.make_move({ .from = { chess::board::File::E, chess::board::Rank::Two },
+        .to                    = { chess::board::File::E, chess::board::Rank::Four },
+        .piece                 = chess::pieces::Type::WhitePawn });
+
+    std::println("{}", print_utf8(position));
+}
