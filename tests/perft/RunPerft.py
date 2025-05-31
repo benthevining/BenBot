afaa4d6e5fab84b931f96007f426e97d3717791e
Ben Vining <ben.the.vining@gmail.com>
2025-05-31 16:23:22 -0500

    test: updated run_perft system to be able to read starting FEN from JSON file

--- tests/perft/RunPerft.py
+++ tests/perft/RunPerft.py
@@ -11,69 +11,60 @@
 import json
 import subprocess
 from pathlib import Path
-import sys
 
 TMP_DIR_PATH = Path('@TMP_DIR@')
 
 with open('@CORRECT_RESULTS_FILE@', 'r') as file:
     CORRECT_DATA = json.load(file)
 
-def get_correct_for(depth):
-    for object in CORRECT_DATA:
-        if object['depth'] == depth:
-            return object['results']
+num_failed = 0
+num_passed = 0
+num_positions = 1
 
-    raise ValueError(f"Correct data for depth {depth} not found")
+for position in CORRECT_DATA:
+    startingFEN = position['position']
 
-ABS_MAX_DEPTH = 6
-MAX_DEPTH = min(@PERFT_DEPTH@, ABS_MAX_DEPTH)
+    print(f'Running tests for position {startingFEN}')
 
-print(f'Max depth: {MAX_DEPTH}')
+    for depthObj in position['depths']:
+        depth = depthObj['depth']
+        correctResultObj = depthObj['results']
 
-# We stop the test if we get the incorrect number of total nodes, since this
-# indicates a total failure of our move generation algorithm. If the stats
-# breakdown is incorrect but the total number of nodes is correct, continue
-# testing deeper depths until the total number of nodes is wrong.
-incorrect_stats = []
-passed = []
+        results_file = TMP_DIR_PATH / f'position_{num_positions}' / f'{depth}_results.json'
 
-for depth in range(MAX_DEPTH + 1):
-    results_file = TMP_DIR_PATH / f'{depth}_results.json'
+        subprocess.run(['$<TARGET_FILE:perft>', f'{depth}', '--fen', startingFEN, '--write-json', results_file])
 
-    subprocess.run(['$<TARGET_FILE:perft>', f'{depth}', '--write-json', results_file])
+        with open(results_file, 'r') as file:
+            result_data = json.load(file)
 
-    with open(results_file, 'r') as file:
-        result_file_text = json.load(file)
+        generatedResultObj = result_data['results']
 
-    results = result_file_text['results']
+        anyError = False
 
-    correct = get_correct_for(depth)
+        for field in ['totalNodes', 'captures', 'castles', 'checks', 'checkmates', 'stalemates', 'en_passants', 'promotions']:
+            expected = correctResultObj[field]
+            reported = generatedResultObj[field]
 
-    any_error = False
+            # We stop the test if we get the incorrect number of total nodes, since this
+            # indicates a total failure of our move generation algorithm. If the stats
+            # breakdown is incorrect but the total number of nodes is correct, continue
+            # testing deeper depths until the total number of nodes is wrong.
 
-    for field in ['totalNodes', 'captures', 'castles', 'checks', 'checkmates', 'stalemates', 'en_passants', 'promotions']:
-        expected = correct[field]
-        reported = results[field]
+            if expected != reported:
+                if field == 'totalNodes': # wrong number of total nodes reported, exit the test with failure
+                    print(f'FATAL ERROR: Depth {depth} reported incorrect number of nodes! Expected {expected}, got {reported}')
+                    exit(1)
 
-        if expected != reported:
-            if field == 'totalNodes': # wrong number of total nodes reported, exit the test with failure
-                print(f'ERROR: Depth {depth} reported incorrect number of nodes! Expected {expected}, got {reported}')
-                exit(1)
+                # wrong stats reported, report failure but continue on to deeper depths
+                print(f'ERROR: Expected {expected} {field}, got {reported}')
+                anyError = True
 
-            # wrong stats reported, report failure but continue on to deeper depths
-            print(f'ERROR: Expected {expected} {field}, got {reported}')
+        if anyError:
+            num_failed += 1
+        else:
+            num_passed += 1
 
-            incorrect_stats.append(depth)
+    num_positions += 1
 
-            any_error = True
-
-    if not any_error:
-        passed.append(depth)
-
-if incorrect_stats:
-    failed_depths = list(set(incorrect_stats)) # remove duplicates
-    failed_depths.sort()
-    print(f'The following depths reported correct total nodes, but incorrect breakdown stats: {failed_depths}')
-
-if passed:
-    print(f'The following depths succeeded: {passed}')
+print(f'{num_passed} test cases passed')
+print(f'{num_failed} test cases failed (correct total nodes, incorrect stats breakdown)')
