d7f186850174f64b24e61aec8dc070d96305907c
Ben Vining <ben.the.vining@gmail.com>
2025-05-30 21:02:51 -0500

    refactor: moved knight_distance() function to cpp file (#57)

--- libchess/include/libchess/board/Distances.hpp
+++ libchess/include/libchess/board/Distances.hpp
@@ -15,10 +15,6 @@
 #pragma once
 
 #include <algorithm>
-#include <array>
-#include <cassert>
-#include <cmath>   // IWYU pragma: keep - for std::abs()
-#include <cstddef> // IWYU pragma: keep - for size_t
 #include <libchess/board/BitboardIndex.hpp>
 #include <libchess/board/Square.hpp>
 #include <utility>
@@ -92,7 +88,7 @@ namespace chess::board {
 
     @relates Square
  */
-[[nodiscard, gnu::const]] inline BitboardIndex knight_distance(
+[[nodiscard, gnu::const]] BitboardIndex knight_distance(
     const Square& first, const Square& second);
 
 /// @}
@@ -155,61 +151,6 @@ constexpr BitboardIndex chebyshev_distance(
     const auto rankDist = rank_distance(first, second);
 
     return std::max(fileDist, rankDist);
-}
-
-inline BitboardIndex knight_distance(
-    const Square& first, const Square& second)
-{
-    using std::size_t;
-
-    static constexpr std::array ndis {
-        0, 3, 2, 3, 2, 3, 4, 5,
-        3, 2, 1, 2, 3, 4, 3, 4,
-        2, 1, 4, 3, 2, 3, 4, 5,
-        3, 2, 3, 2, 3, 4, 3, 4,
-        2, 3, 2, 3, 4, 3, 4, 5,
-        3, 4, 3, 4, 3, 4, 5, 4,
-        4, 3, 4, 3, 4, 5, 4, 5,
-        5, 4, 5, 4, 5, 4, 5, 6
-    };
-
-    static constexpr std::array corner {
-        1, 0, 0, 0, 0, 0, 0, 1,
-        0, 0, 0, 0, 0, 0, 0, 0,
-        0, 0, 0, 0, 0, 0, 0, 0,
-        0, 0, 0, 0, 0, 0, 0, 0,
-        0, 0, 0, 0, 0, 0, 0, 0,
-        0, 0, 0, 0, 0, 0, 0, 0,
-        0, 0, 0, 0, 0, 0, 0, 0,
-        1, 0, 0, 0, 0, 0, 0, 1
-    };
-
-    const auto firstIdx  = static_cast<int>(first.index());
-    const auto secondIdx = static_cast<int>(second.index());
-
-    // NB. this isn't the same as manhattan_distance()
-    const auto absDist = [firstIdx, secondIdx] {
-        const auto rankDist = (firstIdx | 7) - (secondIdx | 7);
-        const auto fileDist = (firstIdx & 7) - (secondIdx & 7);
-
-        return std::abs(rankDist) + std::abs(fileDist);
-    }();
-
-    auto dist = ndis.at(static_cast<size_t>(absDist));
-
-    if (std::cmp_equal(absDist, 9)) {
-        [[unlikely]];
-
-        const auto cornerMask = corner.at(static_cast<size_t>(firstIdx))
-                              ^ corner.at(static_cast<size_t>(secondIdx));
-
-        dist += 2 * cornerMask;
-    }
-
-    assert(dist >= 0);
-    assert(dist <= 6);
-
-    return static_cast<BitboardIndex>(dist);
 }
 
 } // namespace chess::board
---
+++ libchess/src/board/Distances.cpp
@@ -0,0 +1,73 @@
+/*
+ * ======================================================================================
+ *
+ * libchess - a chess engine by Ben Vining
+ *
+ * ======================================================================================
+ */
+
+#include <array>
+#include <cassert>
+#include <cmath>   // IWYU pragma: keep - for std::abs()
+#include <cstddef> // IWYU pragma: keep - for size_t
+#include <libchess/board/Distances.hpp>
+#include <utility>
+
+namespace chess::board {
+
+using std::size_t;
+
+BitboardIndex knight_distance(
+    const Square& first, const Square& second)
+{
+    static constexpr std::array ndis {
+        0, 3, 2, 3, 2, 3, 4, 5,
+        3, 2, 1, 2, 3, 4, 3, 4,
+        2, 1, 4, 3, 2, 3, 4, 5,
+        3, 2, 3, 2, 3, 4, 3, 4,
+        2, 3, 2, 3, 4, 3, 4, 5,
+        3, 4, 3, 4, 3, 4, 5, 4,
+        4, 3, 4, 3, 4, 5, 4, 5,
+        5, 4, 5, 4, 5, 4, 5, 6
+    };
+
+    static constexpr std::array corner {
+        1, 0, 0, 0, 0, 0, 0, 1,
+        0, 0, 0, 0, 0, 0, 0, 0,
+        0, 0, 0, 0, 0, 0, 0, 0,
+        0, 0, 0, 0, 0, 0, 0, 0,
+        0, 0, 0, 0, 0, 0, 0, 0,
+        0, 0, 0, 0, 0, 0, 0, 0,
+        0, 0, 0, 0, 0, 0, 0, 0,
+        1, 0, 0, 0, 0, 0, 0, 1
+    };
+
+    const auto firstIdx  = static_cast<int>(first.index());
+    const auto secondIdx = static_cast<int>(second.index());
+
+    // NB. this isn't the same as manhattan_distance()
+    const auto absDist = [firstIdx, secondIdx] {
+        const auto rankDist = (firstIdx | 7) - (secondIdx | 7);
+        const auto fileDist = (firstIdx & 7) - (secondIdx & 7);
+
+        return std::abs(rankDist) + std::abs(fileDist);
+    }();
+
+    auto dist = ndis.at(static_cast<size_t>(absDist));
+
+    if (std::cmp_equal(absDist, 9)) {
+        [[unlikely]];
+
+        const auto cornerMask = corner.at(static_cast<size_t>(firstIdx))
+                              ^ corner.at(static_cast<size_t>(secondIdx));
+
+        dist += 2 * cornerMask;
+    }
+
+    assert(dist >= 0);
+    assert(dist <= 6);
+
+    return static_cast<BitboardIndex>(dist);
+}
+
+} // namespace chess::board
