41af4ec3c6e20d1b08eba9c90653ba8fc7005ddb
Ben Vining <ben.the.vining@gmail.com>
2025-06-04 00:29:22 -0500

    feat: basic time control algorithm (#94)

---
+++ libchess/include/libchess/game/TimeControl.hpp
@@ -0,0 +1,56 @@
+/*
+ * ======================================================================================
+ *
+ * libchess - a chess engine by Ben Vining
+ *
+ * ======================================================================================
+ */
+
+/** @file
+    This file provides an algorithm for deciding how much time to search.
+    @ingroup game
+ */
+
+#pragma once
+
+#include <chrono>
+
+namespace chess::game {
+
+using Milliseconds = std::chrono::milliseconds;
+
+/** Determines how long to cap the next search at, based on our remaining
+    time and increment.
+
+    @ingroup game
+ */
+[[nodiscard, gnu::const]] constexpr Milliseconds determine_search_time(
+    const Milliseconds& timeRemaining, const Milliseconds& increment) noexcept;
+
+/*
+                         ___                           ,--,
+      ,---,            ,--.'|_                ,--,   ,--.'|
+    ,---.'|            |  | :,'             ,--.'|   |  | :
+    |   | :            :  : ' :             |  |,    :  : '    .--.--.
+    |   | |   ,---.  .;__,'  /    ,--.--.   `--'_    |  ' |   /  /    '
+  ,--.__| |  /     \ |  |   |    /       \  ,' ,'|   '  | |  |  :  /`./
+ /   ,'   | /    /  |:__,'| :   .--.  .-. | '  | |   |  | :  |  :  ;_
+.   '  /  |.    ' / |  '  : |__  \__\/: . . |  | :   '  : |__ \  \    `.
+'   ; |:  |'   ;   /|  |  | '.'| ," .--.; | '  : |__ |  | '.'| `----.   \
+|   | '/  ''   |  / |  ;  :    ;/  /  ,.  | |  | '.'|;  :    ;/  /`--'  /__  ___  ___
+|   :    :||   :    |  |  ,   /;  :   .'   \;  :    ;|  ,   /'--'.     /  .\/  .\/  .\
+ \   \  /   \   \  /    ---`-' |  ,     .-./|  ,   /  ---`-'   `--'---'\  ; \  ; \  ; |
+  `----'     `----'             `--`---'     ---`-'                     `--" `--" `--"
+
+ */
+
+constexpr Milliseconds determine_search_time(
+    const Milliseconds& timeRemaining, const Milliseconds& increment) noexcept
+{
+    return Milliseconds {
+        timeRemaining / Milliseconds { 20 }
+        + increment / Milliseconds { 2 }
+    };
+}
+
+} // namespace chess::game
