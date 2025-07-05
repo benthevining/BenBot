813cd52c71b51e4ff8027b540a9f3cf024720c38
Ben Vining <ben.the.vining@gmail.com>
2025-07-05 00:59:40 -0500

    refactor(search): moved time management logic into separate header

--- libbenbot/src/search/Search.cpp
+++ libbenbot/src/search/Search.cpp
@@ -12,7 +12,8 @@
  * ======================================================================================
  */
 
-#include "MoveOrdering.hpp" // NOLINT(build/include_subdir)
+#include "MoveOrdering.hpp"   // NOLINT(build/include_subdir)
+#include "TimeManagement.hpp" // NOLINT(build/include_subdir)
 #include <algorithm>
 #include <atomic>
 #include <cassert>
@@ -34,7 +35,6 @@
 #include <ranges>
 #include <stdexcept>
 #include <string>
-#include <type_traits>
 #include <utility>
 #include <vector>
 
@@ -106,47 +106,6 @@ namespace {
                 .beta  = -alpha
             };
         }
-    };
-
-    // times the search and also watches the "exit" flag
-    struct Interrupter final {
-        Interrupter(
-            const std::atomic_bool&           exitFlagToUse,
-            const std::optional<Milliseconds> maxSearchTime)
-            : exitFlag { exitFlagToUse }
-            , searchTime { maxSearchTime }
-        {
-        }
-
-        [[nodiscard]] Milliseconds get_search_duration() const
-        {
-            return std::chrono::duration_cast<Milliseconds>(Clock::now() - startTime);
-        }
-
-        [[nodiscard]] bool should_exit() const
-        {
-            if (exitFlag.load())
-                return true;
-
-            if (! searchTime.has_value())
-                return false;
-
-            return get_search_duration() >= *searchTime;
-        }
-
-    private:
-        // On some systems, high_resolution_clock can be unsteady,
-        // in which case it's better to fall back to steady_clock
-        using Clock = std::conditional_t<
-            std::chrono::high_resolution_clock::is_steady,
-            std::chrono::high_resolution_clock,
-            std::chrono::steady_clock>;
-
-        const std::atomic_bool& exitFlag;
-
-        std::chrono::time_point<Clock> startTime { Clock::now() };
-
-        std::optional<Milliseconds> searchTime;
     };
 
     // searches only captures, with no depth limit, to try to
@@ -462,27 +421,6 @@ end_search:
 template struct Context<true>;
 template struct Context<false>;
 
-namespace {
-
-    [[nodiscard]] Milliseconds determine_search_time(
-        const Milliseconds                timeRemaining,
-        const std::optional<Milliseconds> increment,
-        const std::optional<size_t>       movesToNextTimeControl)
-    {
-        const auto inc = increment.value_or(Milliseconds { 0 });
-
-        const auto movesToGo = movesToNextTimeControl.value_or(40uz);
-
-        assert(movesToGo > 0uz);
-
-        return Milliseconds {
-            (timeRemaining.count() / movesToGo)
-            + (inc.count() / (movesToGo / 10uz))
-        };
-    }
-
-} // namespace
-
 void Options::update_from(uci::GoCommandOptions&& goOptions)
 {
     // always clear this, because if movesToSearch isn't specified, we
@@ -514,6 +452,8 @@ void Options::update_from(uci::GoCommandOptions&& goOptions)
                 *timeLeft,
                 isWhite ? goOptions.whiteInc : goOptions.blackInc,
                 goOptions.movesToGo);
+        } else {
+            assert(false); // TODO: ??
         }
     }
 }
---
+++ libbenbot/src/search/TimeManagement.hpp
@@ -0,0 +1,88 @@
+/*
+ * ======================================================================================
+ *
+ * ░▒▓███████▓▒░░▒▓████████▓▒░▒▓███████▓▒░       ░▒▓███████▓▒░ ░▒▓██████▓▒░▒▓████████▓▒░
+ * ░▒▓█▓▒░░▒▓█▓▒░▒▓█▓▒░      ░▒▓█▓▒░░▒▓█▓▒░      ░▒▓█▓▒░░▒▓█▓▒░▒▓█▓▒░░▒▓█▓▒░ ░▒▓█▓▒░
+ * ░▒▓█▓▒░░▒▓█▓▒░▒▓█▓▒░      ░▒▓█▓▒░░▒▓█▓▒░      ░▒▓█▓▒░░▒▓█▓▒░▒▓█▓▒░░▒▓█▓▒░ ░▒▓█▓▒░
+ * ░▒▓███████▓▒░░▒▓██████▓▒░ ░▒▓█▓▒░░▒▓█▓▒░      ░▒▓███████▓▒░░▒▓█▓▒░░▒▓█▓▒░ ░▒▓█▓▒░
+ * ░▒▓█▓▒░░▒▓█▓▒░▒▓█▓▒░      ░▒▓█▓▒░░▒▓█▓▒░      ░▒▓█▓▒░░▒▓█▓▒░▒▓█▓▒░░▒▓█▓▒░ ░▒▓█▓▒░
+ * ░▒▓█▓▒░░▒▓█▓▒░▒▓█▓▒░      ░▒▓█▓▒░░▒▓█▓▒░      ░▒▓█▓▒░░▒▓█▓▒░▒▓█▓▒░░▒▓█▓▒░ ░▒▓█▓▒░
+ * ░▒▓███████▓▒░░▒▓████████▓▒░▒▓█▓▒░░▒▓█▓▒░      ░▒▓███████▓▒░ ░▒▓██████▓▒░  ░▒▓█▓▒░
+ *
+ * ======================================================================================
+ */
+
+#pragma once
+
+#include <atomic>
+#include <cassert>
+#include <chrono>
+#include <cstddef> // IWYU pragma: keep - for size_t
+#include <optional>
+#include <type_traits>
+
+namespace chess::search {
+
+using Milliseconds = std::chrono::milliseconds;
+
+using std::size_t;
+
+// times the search and also watches the "exit" flag
+struct Interrupter final {
+    Interrupter(
+        const std::atomic_bool&           exitFlagToUse,
+        const std::optional<Milliseconds> maxSearchTime)
+        : exitFlag { exitFlagToUse }
+        , searchTime { maxSearchTime }
+    {
+    }
+
+    [[nodiscard]] Milliseconds get_search_duration() const
+    {
+        return std::chrono::duration_cast<Milliseconds>(Clock::now() - startTime);
+    }
+
+    [[nodiscard]] bool should_exit() const
+    {
+        if (exitFlag.load())
+            return true;
+
+        if (! searchTime.has_value())
+            return false;
+
+        return get_search_duration() >= *searchTime;
+    }
+
+private:
+    // On some systems, high_resolution_clock can be unsteady,
+    // in which case it's better to fall back to steady_clock
+    using Clock = std::conditional_t<
+        std::chrono::high_resolution_clock::is_steady,
+        std::chrono::high_resolution_clock,
+        std::chrono::steady_clock>;
+
+    const std::atomic_bool& exitFlag;
+
+    std::chrono::time_point<Clock> startTime { Clock::now() };
+
+    std::optional<Milliseconds> searchTime;
+};
+
+[[nodiscard, gnu::const]] inline Milliseconds determine_search_time(
+    const Milliseconds                timeRemaining,
+    const std::optional<Milliseconds> increment,
+    const std::optional<size_t>       movesToNextTimeControl)
+{
+    const auto inc = increment.value_or(Milliseconds { 0 });
+
+    const auto movesToGo = movesToNextTimeControl.value_or(40uz);
+
+    assert(movesToGo > 0uz);
+
+    return Milliseconds {
+        (timeRemaining.count() / movesToGo)
+        + (inc.count() / (movesToGo / 10uz))
+    };
+}
+
+} // namespace chess::search
