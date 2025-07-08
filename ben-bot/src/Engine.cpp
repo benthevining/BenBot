0bbb7d611b0050342c5e3377ae20a868db85141c
Ben Vining <ben.the.vining@gmail.com>
2025-07-08 06:48:46 -0500

    refactor: reorganizing ben-bot sources

---
+++ ben-bot/src/Engine.cpp
@@ -0,0 +1,130 @@
+/*
+ * ======================================================================================
+ *
+ * ░▒▓███████▓▒░░▒▓████████▓▒░▒▓███████▓▒░       ░▒▓███████▓▒░ ░▒▓██████▓▒░▒▓████████▓▒░
+ * ░▒▓█▓▒░░▒▓█▓▒░▒▓█▓▒░      ░▒▓█▓▒░░▒▓█▓▒░      ░▒▓█▓▒░░▒▓█▓▒░▒▓█▓▒░░▒▓█▓▒░ ░▒▓█▓▒░
+ * ░▒▓█▓▒░░▒▓█▓▒░▒▓█▓▒░      ░▒▓█▓▒░░▒▓█▓▒░      ░▒▓█▓▒░░▒▓█▓▒░▒▓█▓▒░░▒▓█▓▒░ ░▒▓█▓▒░
+ * ░▒▓███████▓▒░░▒▓██████▓▒░ ░▒▓█▓▒░░▒▓█▓▒░      ░▒▓███████▓▒░░▒▓█▓▒░░▒▓█▓▒░ ░▒▓█▓▒░
+ * ░▒▓█▓▒░░▒▓█▓▒░▒▓█▓▒░      ░▒▓█▓▒░░▒▓█▓▒░      ░▒▓█▓▒░░▒▓█▓▒░▒▓█▓▒░░▒▓█▓▒░ ░▒▓█▓▒░
+ * ░▒▓█▓▒░░▒▓█▓▒░▒▓█▓▒░      ░▒▓█▓▒░░▒▓█▓▒░      ░▒▓█▓▒░░▒▓█▓▒░▒▓█▓▒░░▒▓█▓▒░ ░▒▓█▓▒░
+ * ░▒▓███████▓▒░░▒▓████████▓▒░▒▓█▓▒░░▒▓█▓▒░      ░▒▓███████▓▒░ ░▒▓██████▓▒░  ░▒▓█▓▒░
+ *
+ * ======================================================================================
+ */
+
+#include "Engine.hpp" // NOLINT(build/include_subdir)
+#include <cmrc/cmrc.hpp>
+#include <exception>
+#include <filesystem>
+#include <fstream>
+#include <ios>
+#include <iterator>
+#include <libchess/moves/Magics.hpp>
+#include <print>
+#include <string>
+#include <string_view>
+
+CMRC_DECLARE(ben_bot_resources);
+
+namespace chess {
+
+void BenBotEngine::new_game()
+{
+    moves::magics::init();
+
+    searcher.context.reset(); // clears transposition table
+
+    if (! bookLoaded) { // load embedded book data into opening book data structure
+        const auto bookFile = cmrc::ben_bot_resources::get_filesystem()
+                                  .open("book.json");
+
+        searcher.context.openingBook.book.add_from_json(std::string_view { bookFile });
+
+        bookLoaded = true;
+    }
+}
+
+namespace {
+    [[nodiscard]] std::string load_file_as_string(const std::filesystem::path& file)
+    {
+        std::ifstream input { file };
+
+        input.exceptions(
+            std::ios_base::badbit | std::ios_base::failbit);
+
+        using Iterator = std::istreambuf_iterator<char>;
+
+        return { Iterator { input }, Iterator {} };
+    }
+} // namespace
+
+using std::println;
+
+// this function implements non-standard UCI commands that we support
+void BenBotEngine::handle_custom_command(
+    const std::string_view command, const std::string_view options)
+{
+    if (command == "loadbook") {
+        load_book_file(std::filesystem::path { options });
+        return;
+    }
+
+    if (command == "options") {
+        print_options();
+        return;
+    }
+
+    if (command == "help") {
+        print_help();
+        return;
+    }
+
+    println("Unknown UCI command: {}", command);
+    println("Type 'help' for a list of supported commands");
+}
+
+void BenBotEngine::load_book_file(const std::filesystem::path& file)
+try {
+    wait();
+
+    searcher.context.openingBook.book.add_from_json(
+        load_file_as_string(file));
+} catch (const std::exception& except) {
+    println(
+        "Error reading from opening book file at path: {}",
+        file.string());
+
+    println("{}", except.what());
+}
+
+void BenBotEngine::print_help() const
+{
+    println(
+        "{} by {}",
+        get_name(), get_author());
+
+    println(
+        "All standard UCI commands are supported, as well as the following non-standard commands:");
+
+    println(
+        "loadbook <path> - reads the given JSON file into the engine's openings database. See book.json in the ben-bot source code for an example of the format.");
+    println(
+        "options         - dump current UCI option values");
+    println(
+        "help            - displays this text");
+}
+
+void BenBotEngine::print_options() const
+{
+    const auto& ownBook = searcher.context.openingBook.enabled;
+
+    println(
+        "{} - toggle - controls whether internal opening book is used - current {} - default {}",
+        ownBook.get_name(), ownBook.get_value(), ownBook.get_default_value());
+
+    println(
+        "{} - button - press to clear the transposition table",
+        clearTT.get_name());
+}
+
+} // namespace chess
---
+++ ben-bot/src/Engine.hpp
@@ -0,0 +1,71 @@
+/*
+ * ======================================================================================
+ *
+ * ░▒▓███████▓▒░░▒▓████████▓▒░▒▓███████▓▒░       ░▒▓███████▓▒░ ░▒▓██████▓▒░▒▓████████▓▒░
+ * ░▒▓█▓▒░░▒▓█▓▒░▒▓█▓▒░      ░▒▓█▓▒░░▒▓█▓▒░      ░▒▓█▓▒░░▒▓█▓▒░▒▓█▓▒░░▒▓█▓▒░ ░▒▓█▓▒░
+ * ░▒▓█▓▒░░▒▓█▓▒░▒▓█▓▒░      ░▒▓█▓▒░░▒▓█▓▒░      ░▒▓█▓▒░░▒▓█▓▒░▒▓█▓▒░░▒▓█▓▒░ ░▒▓█▓▒░
+ * ░▒▓███████▓▒░░▒▓██████▓▒░ ░▒▓█▓▒░░▒▓█▓▒░      ░▒▓███████▓▒░░▒▓█▓▒░░▒▓█▓▒░ ░▒▓█▓▒░
+ * ░▒▓█▓▒░░▒▓█▓▒░▒▓█▓▒░      ░▒▓█▓▒░░▒▓█▓▒░      ░▒▓█▓▒░░▒▓█▓▒░▒▓█▓▒░░▒▓█▓▒░ ░▒▓█▓▒░
+ * ░▒▓█▓▒░░▒▓█▓▒░▒▓█▓▒░      ░▒▓█▓▒░░▒▓█▓▒░      ░▒▓█▓▒░░▒▓█▓▒░▒▓█▓▒░░▒▓█▓▒░ ░▒▓█▓▒░
+ * ░▒▓███████▓▒░░▒▓████████▓▒░▒▓█▓▒░░▒▓█▓▒░      ░▒▓███████▓▒░ ░▒▓██████▓▒░  ░▒▓█▓▒░
+ *
+ * ======================================================================================
+ */
+
+#pragma once
+
+#include <array>
+#include <filesystem>
+#include <libbenbot/search/Search.hpp>
+#include <libbenbot/search/Thread.hpp>
+#include <libchess/game/Position.hpp>
+#include <libchess/uci/CommandParsing.hpp> // IWYU pragma: keep - for uci::GoCommandOptions
+#include <libchess/uci/EngineBase.hpp>
+#include <libchess/uci/Options.hpp>
+#include <span>
+#include <string_view>
+#include <utility>
+
+namespace chess {
+
+class BenBotEngine final : public uci::EngineBase {
+    [[nodiscard]] std::string_view get_name() const override { return "BenBot"; }
+
+    [[nodiscard]] std::string_view get_author() const override { return "Ben Vining"; }
+
+    void new_game() override;
+
+    void set_position(const game::Position& pos) override { searcher.set_position(pos); }
+
+    void go(uci::GoCommandOptions&& opts) override { searcher.start(std::move(opts)); }
+
+    void abort_search() override { searcher.context.abort(); }
+
+    void wait() override { searcher.context.wait(); }
+
+    [[nodiscard]] std::span<uci::Option*> get_options() override { return options; }
+
+    void handle_custom_command(std::string_view command, std::string_view options) override;
+
+    void load_book_file(const std::filesystem::path& file);
+
+    void print_help() const;
+
+    void print_options() const;
+
+    search::Thread searcher { search::Callbacks::make_uci_handler() };
+
+    bool bookLoaded { false };
+
+    uci::Action clearTT {
+        "Clear Hash",
+        [this] { searcher.context.reset(); }
+    };
+
+    std::array<uci::Option*, 2uz> options {
+        &searcher.context.openingBook.enabled,
+        &clearTT
+    };
+};
+
+} // namespace chess
