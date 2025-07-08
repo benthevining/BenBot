cb8822ac2697d9e52b575b54737c389074a6a7bc
Ben Vining <ben.the.vining@gmail.com>
2025-07-08 16:27:15 -0500

    refactor(ben-bot): resource loading

--- ben-bot/src/Data.cpp
+++ ben-bot/src/Data.cpp
@@ -15,26 +15,31 @@
 #include "Data.hpp"
 #include "BenBotVersion.hpp"
 #include <cmrc/cmrc.hpp>
+#include <string>
 #include <string_view>
 
 CMRC_DECLARE(ben_bot_resources);
 
 namespace ben_bot {
 
+namespace {
+    [[nodiscard]] string_view get_named_resource(const std::string& name)
+    {
+        const auto file = cmrc::ben_bot_resources::get_filesystem()
+                              .open(name);
+
+        return string_view { file }; // NOLINT
+    }
+} // namespace
+
 string_view get_opening_book_json_text()
 {
-    const auto bookFile = cmrc::ben_bot_resources::get_filesystem()
-                              .open("book.json");
-
-    return string_view { bookFile }; // NOLINT
+    return get_named_resource("book.json");
 }
 
 string_view get_ascii_logo()
 {
-    const auto file = cmrc::ben_bot_resources::get_filesystem()
-                          .open("license_header.txt");
-
-    return string_view { file }; // NOLINT
+    return get_named_resource("license_header.txt");
 }
 
 string_view get_version_string()
