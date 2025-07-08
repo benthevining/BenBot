956158d0b4bd81d6c1c12858247835ec3f14c143
Ben Vining <ben.the.vining@gmail.com>
2025-07-08 05:58:29 -0500

    feat(uci): loadbook command to load openings from a given file (#163)

--- ben-bot/main.cpp
+++ ben-bot/main.cpp
@@ -16,6 +16,10 @@
 #include <cmrc/cmrc.hpp>
 #include <cstdlib>
 #include <exception>
+#include <filesystem>
+#include <fstream>
+#include <ios>
+#include <iterator>
 #include <libbenbot/data-structures/OpeningBook.hpp>
 #include <libbenbot/search/Search.hpp>
 #include <libbenbot/search/Thread.hpp>
@@ -26,17 +30,34 @@
 #include <libchess/uci/Options.hpp>
 #include <print>
 #include <span>
+#include <string>
 #include <string_view>
 #include <utility>
 
 CMRC_DECLARE(ben_bot_resources);
 
+namespace {
+[[nodiscard]] std::string load_file_as_string(const std::filesystem::path& file)
+{
+    std::ifstream input { file };
+
+    input.exceptions(
+        std::ios_base::badbit | std::ios_base::failbit);
+
+    using Iterator = std::istreambuf_iterator<char>;
+
+    return { Iterator { input }, Iterator {} };
+}
+} // namespace
+
 namespace chess {
 
-class BenBotEngine final : public uci::EngineBase {
-    [[nodiscard]] std::string_view get_name() const override { return "BenBot"; }
+using std::string_view;
 
-    [[nodiscard]] std::string_view get_author() const override { return "Ben Vining"; }
+class BenBotEngine final : public uci::EngineBase {
+    [[nodiscard]] string_view get_name() const override { return "BenBot"; }
+
+    [[nodiscard]] string_view get_author() const override { return "Ben Vining"; }
 
     void new_game() override
     {
@@ -48,10 +69,17 @@ class BenBotEngine final : public uci::EngineBase {
             const auto bookFile = cmrc::ben_bot_resources::get_filesystem()
                                       .open("book.json");
 
-            searcher.context.openingBook.book.add_from_json(std::string_view { bookFile });
+            searcher.context.openingBook.book.add_from_json(string_view { bookFile });
 
             bookLoaded = true;
         }
+    }
+
+    // this function implements non-standard UCI commands that we support
+    void handle_custom_command(const string_view command, const string_view options) override
+    {
+        if (command == "loadbook")
+            load_book_file(std::filesystem::path { options });
     }
 
     void set_position(const game::Position& pos) override { searcher.set_position(pos); }
@@ -63,6 +91,20 @@ class BenBotEngine final : public uci::EngineBase {
     void wait() override { searcher.context.wait(); }
 
     [[nodiscard]] std::span<uci::Option*> get_options() override { return options; }
+
+    void load_book_file(const std::filesystem::path& file)
+    try {
+        wait();
+
+        searcher.context.openingBook.book.add_from_json(
+            load_file_as_string(file));
+    } catch (const std::exception& except) {
+        std::println(
+            "Error reading from opening book file at path: {}",
+            file.string());
+
+        std::println("{}", except.what());
+    }
 
     search::Thread searcher { search::Callbacks::make_uci_handler() };
 
