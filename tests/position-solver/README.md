1f77804276a4ce247ce7b103190e3e0ebe2568e0
Ben Vining <ben.the.vining@gmail.com>
2025-07-13 04:01:01 -0500

    refactor(tests): position solver tests are now launched in parallel by ctest

--- tests/position-solver/README.md
+++ tests/position-solver/README.md
@@ -2,8 +2,6 @@
 
 This directory contains a simple executable that reads test positions with known best moves from EPD files, and exits with an error if the `ben_bot` search algorithm doesn't find the correct move.
 
-Each EPD file may contain multiple positions, and will be executed as one CTest test case.
-
 Each EPD entry must have the following operations:
 * `bm`: best move, in algebraic (SAN) notation
 * `depth`: search depth. It is desirable to set this as low as possible, while still getting the correct answer. When adding new test positions, a good strategy is to start with depth 4; increase this if the test fails (if it still fails with a depth of 6, we likely won't find the move at any reasonable depth), and decrease this if the test passes.
--- tests/position-solver/main.cpp
+++ tests/position-solver/main.cpp
@@ -26,12 +26,32 @@
 #include <libchess/util/Strings.hpp>
 #include <optional>
 #include <print>
+#include <ranges>
 #include <span>
 #include <string>
 #include <string_view>
 #include <vector>
 
 using std::size_t;
+
+namespace {
+void print_usage(const std::string_view programName)
+{
+    std::println("Usage:");
+    std::println("{} <epdPath> <lineIdx>", programName);
+}
+
+[[nodiscard]] std::string_view get_line(
+    const std::string_view text, const size_t lineIdx)
+{
+    const auto lines = text
+                     | std::views::split('\n')
+                     | std::views::transform([](auto range) { return std::string_view { range }; })
+                     | std::ranges::to<std::vector>();
+
+    return lines.at(lineIdx);
+}
+} // namespace
 
 int main(const int argc, const char** argv)
 try {
@@ -50,10 +70,24 @@ try {
     args = args.subspan(1uz);
 
     if (args.empty()) {
-        std::println("Usage:");
-        std::println("{} <epdPath>", programName);
+        print_usage(programName);
         return EXIT_FAILURE;
     }
+
+    const auto fileContent = chess::util::load_file_as_string(
+        std::filesystem::path { args.front() });
+
+    args = args.subspan(1uz);
+
+    if (args.empty()) {
+        print_usage(programName);
+        return EXIT_FAILURE;
+    }
+
+    const auto lineIdx = chess::util::int_from_string<size_t>(args.front());
+
+    const auto epd = notation::from_epd(
+        get_line(fileContent, lineIdx));
 
     std::optional<chess::moves::Move> foundMove;
 
@@ -64,53 +98,32 @@ try {
             } }
     };
 
-    const auto fileContent = chess::util::load_file_as_string(
-        std::filesystem::path { args.front() });
+    context.options.position = epd.position;
 
-    size_t numPassed { 0uz };
-    size_t numFailed { 0uz };
+    context.options.depth = chess::util::int_from_string(
+        epd.operations.at("depth"), 4uz);
 
-    for (const auto& epd : notation::parse_all_epds(fileContent)) {
-        context.options.position = epd.position;
+    const auto expectedMove = notation::from_alg(
+        epd.position, epd.operations.at("bm"));
 
-        // clear this so that all legal moves in the position will be searched
-        context.options.movesToSearch.clear();
+    context.search();
 
-        context.options.depth = chess::util::int_from_string(
-            epd.operations.at("depth"), 4uz);
-
-        const auto expectedMove = notation::from_alg(
-            epd.position, epd.operations.at("bm"));
-
-        foundMove.reset();
-
-        context.clear_transposition_table();
-
-        context.search();
-
-        if (foundMove.value() == expectedMove) {
-            ++numPassed;
-            continue;
-        }
-
-        ++numFailed;
-
-        std::println(std::cerr,
-            "Position failed: {} ({})",
-            notation::to_fen(epd.position),
-            epd.operations.at("id"));
-
-        std::println(std::cerr,
-            "Expected {}, got {}",
-            notation::to_alg(epd.position, expectedMove),
-            notation::to_alg(epd.position, *foundMove));
+    if (foundMove.value() == expectedMove) {
+        std::println("Passed!");
+        return EXIT_SUCCESS;
     }
 
-    std::println(
-        "{} test cases passed, {} test cases failed",
-        numPassed, numFailed);
+    std::println(std::cerr,
+        "Position failed: {} ({})",
+        notation::to_fen(epd.position),
+        epd.operations.at("id"));
 
-    return numFailed;
+    std::println(std::cerr,
+        "Expected {}, got {}",
+        notation::to_alg(epd.position, expectedMove),
+        notation::to_alg(epd.position, *foundMove));
+
+    return EXIT_FAILURE;
 } catch (const std::exception& exception) {
     std::println(std::cerr, "{}", exception.what());
     return EXIT_FAILURE;
