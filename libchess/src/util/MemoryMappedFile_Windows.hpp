3726f0e3099e8cdbd20cf2dc25b73d40d3e87e96
Ben Vining <ben.the.vining@gmail.com>
2025-07-14 18:19:33 -0500

    refactor: memory mapped file

--- libchess/src/util/MemoryMappedFile_Windows.hpp
+++ libchess/src/util/MemoryMappedFile_Windows.hpp
@@ -21,6 +21,7 @@
 #include <cstddef> // IWYU pragma: keep - for size_t
 #include <filesystem>
 #include <libchess/util/Files.hpp>
+#include <utility>
 #include <windows.h>
 
 namespace chess::util {
@@ -31,41 +32,38 @@ struct MemoryMappedFile::Pimpl final {
     Pimpl(
         const std::filesystem::path& file, const bool exclusive)
         : fileSize { static_cast<size_t>(file_size(file)) }
+        , fileHandle {
+            CreateFileA(
+                file.string().c_str(), // NOLINT(build/include_what_you_use)
+                GENERIC_READ,
+                exclusive ? 0 : (FILE_SHARE_READ | FILE_SHARE_DELETE),
+                nullptr, OPEN_EXISTING,
+                FILE_ATTRIBUTE_NORMAL | FILE_FLAG_SEQUENTIAL_SCAN,
+                nullptr)
+        }
     {
-        static constexpr DWORD accessMode { GENERIC_READ };
-        static constexpr DWORD createType { OPEN_EXISTING };
-        static constexpr DWORD protect { PAGE_READONLY };
-        static constexpr DWORD access { FILE_MAP_READ };
+        if (fileHandle == INVALID_HANDLE_VALUE) {
+            fileSize = 0uz;
+            return;
+        }
 
-        auto* handle = CreateFileA(
-            file.string().c_str(), // NOLINT(build/include_what_you_use)
-            accessMode,
-            exclusive ? 0 : (FILE_SHARE_READ | FILE_SHARE_DELETE),
-            nullptr, createType,
-            FILE_ATTRIBUTE_NORMAL | FILE_FLAG_SEQUENTIAL_SCAN,
+        auto* mappingHandle = CreateFileMapping(
+            handle, nullptr, PAGE_READONLY,
+            static_cast<DWORD>(fileSize >> 32uz),
+            static_cast<DWORD>(fileSize),
             nullptr);
 
-        if (handle != INVALID_HANDLE_VALUE) {
-            fileHandle = handle;
+        if (mappingHandle != nullptr) {
+            static constexpr auto zero { static_cast<DWORD>(0) };
 
-            auto* mappingHandle = CreateFileMapping(
-                handle, nullptr, protect,
-                static_cast<DWORD>(fileSize >> 32uz),
-                static_cast<DWORD>(fileSize),
-                nullptr);
+            address = MapViewOfFile(
+                mappingHandle, FILE_MAP_READ, zero, zero,
+                static_cast<SIZE_T>(fileSize));
 
-            if (mappingHandle != nullptr) {
-                address = MapViewOfFile(
-                    mappingHandle, access,
-                    static_cast<DWORD>(0uz),
-                    static_cast<DWORD>(0uz),
-                    static_cast<SIZE_T>(fileSize));
+            if (address == nullptr)
+                fileSize = 0uz;
 
-                if (address == nullptr)
-                    fileSize = 0uz;
-
-                CloseHandle(mappingHandle);
-            }
+            CloseHandle(mappingHandle);
         }
     }
 
@@ -80,8 +78,22 @@ struct MemoryMappedFile::Pimpl final {
 
     Pimpl(const Pimpl&)            = delete;
     Pimpl& operator=(const Pimpl&) = delete;
-    Pimpl(Pimpl&&)                 = delete;
-    Pimpl& operator=(Pimpl&&)      = delete;
+
+    Pimpl(Pimpl&& other) noexcept
+        : fileSize { std::exchange(other.fileSize, 0uz) }
+        , fileHandle { std::exchange(other.fileHandle, INVALID_HANDLE_VALUE) }
+        , address { std::exchange(other.address, nullptr) }
+    {
+    }
+
+    Pimpl& operator=(Pimpl&& other) noexcept
+    {
+        fileSize   = std::exchange(other.fileSize, 0uz);
+        fileHandle = std::exchange(other.fileHandle, INVALID_HANDLE_VALUE);
+        address    = std::exchange(other.address, nullptr);
+
+        return *this;
+    }
 
     [[nodiscard]] std::string_view data() const
     {
@@ -94,7 +106,7 @@ struct MemoryMappedFile::Pimpl final {
 private:
     size_t fileSize;
 
-    HANDLE fileHandle { INVALID_HANDLE_VALUE };
+    HANDLE fileHandle;
 
     void* address { nullptr };
 };
