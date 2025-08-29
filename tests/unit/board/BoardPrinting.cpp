1f2c791ba87ef117cf71972d43df4b43c89282c1
Ben Vining <ben.the.vining@gmail.com>
2025-08-28 19:55:47 -0500

    test: board printing test coverage

---
+++ tests/unit/board/BoardPrinting.cpp
@@ -0,0 +1,38 @@
+/*
+ * ======================================================================================
+ *
+ * ░▒▓███████▓▒░░▒▓████████▓▒░▒▓███████▓▒░       ░▒▓███████▓▒░ ░▒▓██████▓▒░▒▓████████▓▒░
+ * ░▒▓█▓▒░░▒▓█▓▒░▒▓█▓▒░      ░▒▓█▓▒░░▒▓█▓▒░      ░▒▓█▓▒░░▒▓█▓▒░▒▓█▓▒░░▒▓█▓▒░ ░▒▓█▓▒░
+ * ░▒▓█▓▒░░▒▓█▓▒░▒▓█▓▒░      ░▒▓█▓▒░░▒▓█▓▒░      ░▒▓█▓▒░░▒▓█▓▒░▒▓█▓▒░░▒▓█▓▒░ ░▒▓█▓▒░
+ * ░▒▓███████▓▒░░▒▓██████▓▒░ ░▒▓█▓▒░░▒▓█▓▒░      ░▒▓███████▓▒░░▒▓█▓▒░░▒▓█▓▒░ ░▒▓█▓▒░
+ * ░▒▓█▓▒░░▒▓█▓▒░▒▓█▓▒░      ░▒▓█▓▒░░▒▓█▓▒░      ░▒▓█▓▒░░▒▓█▓▒░▒▓█▓▒░░▒▓█▓▒░ ░▒▓█▓▒░
+ * ░▒▓█▓▒░░▒▓█▓▒░▒▓█▓▒░      ░▒▓█▓▒░░▒▓█▓▒░      ░▒▓█▓▒░░▒▓█▓▒░▒▓█▓▒░░▒▓█▓▒░ ░▒▓█▓▒░
+ * ░▒▓███████▓▒░░▒▓████████▓▒░▒▓█▓▒░░▒▓█▓▒░      ░▒▓███████▓▒░ ░▒▓██████▓▒░  ░▒▓█▓▒░
+ *
+ * ======================================================================================
+ */
+
+#include <algorithm>
+#include <catch2/catch_test_macros.hpp>
+#include <libchess/board/Bitboard.hpp>
+#include <libchess/game/Position.hpp>
+#include <string_view>
+
+TEST_CASE("Board printing", "[board][printing]")
+{
+    static constexpr chess::board::Bitboard bitboard;
+
+    static const chess::game::Position position;
+
+    const auto bbASCII  = print_ascii(bitboard);
+    const auto posASCII = print_ascii(position);
+    const auto posUTF8  = print_utf8(position);
+
+    // the number of pipe characters in each board string should be the same
+    auto count_pipe_chars = [](const std::string_view str) {
+        return std::ranges::count(str, '|');
+    };
+
+    REQUIRE(count_pipe_chars(bbASCII) == count_pipe_chars(posASCII));
+    REQUIRE(count_pipe_chars(posASCII) == count_pipe_chars(posUTF8));
+}
