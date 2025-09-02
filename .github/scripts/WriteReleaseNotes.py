24a270c02b3881d6560163ad6a273aee536b6d0a
Ben Vining <ben.the.vining@gmail.com>
2025-09-02 16:37:59 -0500

    feat: releases now include changelog notes in release body text (#263)

---
+++ .github/scripts/WriteReleaseNotes.py
@@ -0,0 +1,35 @@
+# ======================================================================================
+#
+# ░▒▓███████▓▒░░▒▓████████▓▒░▒▓███████▓▒░       ░▒▓███████▓▒░ ░▒▓██████▓▒░▒▓████████▓▒░
+# ░▒▓█▓▒░░▒▓█▓▒░▒▓█▓▒░      ░▒▓█▓▒░░▒▓█▓▒░      ░▒▓█▓▒░░▒▓█▓▒░▒▓█▓▒░░▒▓█▓▒░ ░▒▓█▓▒░
+# ░▒▓█▓▒░░▒▓█▓▒░▒▓█▓▒░      ░▒▓█▓▒░░▒▓█▓▒░      ░▒▓█▓▒░░▒▓█▓▒░▒▓█▓▒░░▒▓█▓▒░ ░▒▓█▓▒░
+# ░▒▓███████▓▒░░▒▓██████▓▒░ ░▒▓█▓▒░░▒▓█▓▒░      ░▒▓███████▓▒░░▒▓█▓▒░░▒▓█▓▒░ ░▒▓█▓▒░
+# ░▒▓█▓▒░░▒▓█▓▒░▒▓█▓▒░      ░▒▓█▓▒░░▒▓█▓▒░      ░▒▓█▓▒░░▒▓█▓▒░▒▓█▓▒░░▒▓█▓▒░ ░▒▓█▓▒░
+# ░▒▓█▓▒░░▒▓█▓▒░▒▓█▓▒░      ░▒▓█▓▒░░▒▓█▓▒░      ░▒▓█▓▒░░▒▓█▓▒░▒▓█▓▒░░▒▓█▓▒░ ░▒▓█▓▒░
+# ░▒▓███████▓▒░░▒▓████████▓▒░▒▓█▓▒░░▒▓█▓▒░      ░▒▓███████▓▒░ ░▒▓██████▓▒░  ░▒▓█▓▒░
+#
+# ======================================================================================
+
+from pathlib import Path
+import sys
+import re
+
+CHANGELOG = Path(sys.argv[1])
+OUTPUT_FILE = Path(sys.argv[2])
+
+with open(CHANGELOG, 'r') as file:
+    changelog_content = file.readlines()
+
+def find_last_version_heading():
+    for idx, line in enumerate(reversed(changelog_content)):
+        if re.match('^(##)+', line):
+            return len(changelog_content) - idx
+
+    raise ValueError('Version heading (##) not found in changelog text')
+
+start_line_idx = find_last_version_heading()
+
+release_notes = changelog_content[start_line_idx:]
+
+with open(OUTPUT_FILE, 'w') as file:
+    file.write(''.join(release_notes))
