859f455c4ab6a6feea9ffe7f5ef33d08f4b57e8f
Ben Vining <ben.the.vining@gmail.com>
2025-09-06 02:51:45 -0500

    fix: release action [skip ci]

--- .github/scripts/ReZipArtifacts.py
+++ .github/scripts/ReZipArtifacts.py
@@ -17,15 +17,19 @@ import shutil
 
 DOWNLOADS_ROOT = Path(sys.argv[1])
 
-# by default the upload-pages-artifact action used by the docs workflow will create a file named 'artifact.tar'
-os.rename(f'{DOWNLOADS_ROOT}/artifact.tar', f'{DOWNLOADS_ROOT}/docs.tar')
-
 for dirpath, dirnames, filenames in os.walk(DOWNLOADS_ROOT):
     for subdir_name in dirnames:
         subdir_path = DOWNLOADS_ROOT / subdir_name
 
-        shutil.make_archive(DOWNLOADS_ROOT / subdir_name, 'zip', subdir_path)
+        shutil.make_archive(subdir_name, 'zip', subdir_path)
 
         os.rmdir(subdir_path)
 
     break
+
+# by default the upload-pages-artifact action used by the docs workflow will create a file named 'artifact.tar'
+docs_path = DOWNLOADS_ROOT / 'artifact'
+
+shutil.make_archive('BenBot-docs', 'zip', docs_path)
+
+os.rmdir(docs_path)
