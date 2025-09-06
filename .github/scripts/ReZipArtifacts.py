28c24738882c2f25ce082cc8f626a30a9204eced
Ben Vining <ben.the.vining@gmail.com>
2025-09-06 02:41:49 -0500

    fix: release action [skip ci]

---
+++ .github/scripts/ReZipArtifacts.py
@@ -0,0 +1,31 @@
+# ======================================================================================
+#
+# ░▒▓███████▓▒░░▒▓████████▓▒░▒▓███████▓▒░       ░▒▓███████▓▒░ ░▒▓██████▓▒░▒▓████████▓▒░
+# ░▒▓█▓▒░░▒▓█▓▒░▒▓█▓▒░      ░▒▓█▓▒░░▒▓█▓▒░      ░▒▓█▓▒░░▒▓█▓▒░▒▓█▓▒░░▒▓█▓▒░ ░▒▓█▓▒░
+# ░▒▓█▓▒░░▒▓█▓▒░▒▓█▓▒░      ░▒▓█▓▒░░▒▓█▓▒░      ░▒▓█▓▒░░▒▓█▓▒░▒▓█▓▒░░▒▓█▓▒░ ░▒▓█▓▒░
+# ░▒▓███████▓▒░░▒▓██████▓▒░ ░▒▓█▓▒░░▒▓█▓▒░      ░▒▓███████▓▒░░▒▓█▓▒░░▒▓█▓▒░ ░▒▓█▓▒░
+# ░▒▓█▓▒░░▒▓█▓▒░▒▓█▓▒░      ░▒▓█▓▒░░▒▓█▓▒░      ░▒▓█▓▒░░▒▓█▓▒░▒▓█▓▒░░▒▓█▓▒░ ░▒▓█▓▒░
+# ░▒▓█▓▒░░▒▓█▓▒░▒▓█▓▒░      ░▒▓█▓▒░░▒▓█▓▒░      ░▒▓█▓▒░░▒▓█▓▒░▒▓█▓▒░░▒▓█▓▒░ ░▒▓█▓▒░
+# ░▒▓███████▓▒░░▒▓████████▓▒░▒▓█▓▒░░▒▓█▓▒░      ░▒▓███████▓▒░ ░▒▓██████▓▒░  ░▒▓█▓▒░
+#
+# ======================================================================================
+
+from pathlib import Path
+import sys
+import os
+import shutil
+
+DOWNLOADS_ROOT = Path(sys.argv[1])
+
+# by default the upload-pages-artifact action used by the docs workflow will create a file named 'artifact.tar'
+os.rename(f'{DOWNLOADS_ROOT}/artifact.tar', f'{DOWNLOADS_ROOT}/docs.tar')
+
+for dirpath, dirnames, filenames in os.walk(DOWNLOADS_ROOT):
+    for subdir_name in dirnames:
+        subdir_path = DOWNLOADS_ROOT / subdir_name
+
+        shutil.make_archive(DOWNLOADS_ROOT / subdir_name, 'zip', subdir_path)
+
+        os.rmdir(subdir_path)
+
+    break
