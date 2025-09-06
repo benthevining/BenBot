3c5d633a1a1afd6dc1d1f1b7605e0a4df6b750cc
Ben Vining <ben.the.vining@gmail.com>
2025-09-06 03:39:23 -0500

    fix: release action [skip ci]

--- .github/scripts/ReZipArtifacts.py
+++ .github/scripts/ReZipArtifacts.py
@@ -16,18 +16,24 @@ import os
 import shutil
 
 DOWNLOADS_ROOT = Path(sys.argv[1])
+OUTPUT_DIR = Path(sys.argv[2])
 
 for dirpath, dirnames, filenames in os.walk(DOWNLOADS_ROOT):
     for subdir_name in dirnames:
+        if subdir_name == 'BenBot-docs':
+            os.rename(
+                DOWNLOADS_ROOT / subdir_name / subdir_name / 'artifact.tar',
+                OUTPUT_DIR / 'BenBot-docs.tar'
+            )
+            continue
+
         subdir_path = DOWNLOADS_ROOT / subdir_name
 
         shutil.make_archive(
-            DOWNLOADS_ROOT / subdir_name,
+            OUTPUT_DIR / subdir_name,
             'zip',
             root_dir=DOWNLOADS_ROOT,
-            base_dir=subdir_path
+            base_dir=subdir_name
         )
-
-        shutil.rmtree(subdir_path)
 
     break
