4a0618613d49dee89450e6299f8ab685563c47c1
Ben Vining <ben.the.vining@gmail.com>
2025-09-13 22:24:55 -0500

    feat: cppcheck integration

---
+++ config/StaticAnalysis.cmake
@@ -0,0 +1,50 @@
+# ======================================================================================
+#
+# ░▒▓███████▓▒░░▒▓████████▓▒░▒▓███████▓▒░       ░▒▓███████▓▒░ ░▒▓██████▓▒░▒▓████████▓▒░
+# ░▒▓█▓▒░░▒▓█▓▒░▒▓█▓▒░      ░▒▓█▓▒░░▒▓█▓▒░      ░▒▓█▓▒░░▒▓█▓▒░▒▓█▓▒░░▒▓█▓▒░ ░▒▓█▓▒░
+# ░▒▓█▓▒░░▒▓█▓▒░▒▓█▓▒░      ░▒▓█▓▒░░▒▓█▓▒░      ░▒▓█▓▒░░▒▓█▓▒░▒▓█▓▒░░▒▓█▓▒░ ░▒▓█▓▒░
+# ░▒▓███████▓▒░░▒▓██████▓▒░ ░▒▓█▓▒░░▒▓█▓▒░      ░▒▓███████▓▒░░▒▓█▓▒░░▒▓█▓▒░ ░▒▓█▓▒░
+# ░▒▓█▓▒░░▒▓█▓▒░▒▓█▓▒░      ░▒▓█▓▒░░▒▓█▓▒░      ░▒▓█▓▒░░▒▓█▓▒░▒▓█▓▒░░▒▓█▓▒░ ░▒▓█▓▒░
+# ░▒▓█▓▒░░▒▓█▓▒░▒▓█▓▒░      ░▒▓█▓▒░░▒▓█▓▒░      ░▒▓█▓▒░░▒▓█▓▒░▒▓█▓▒░░▒▓█▓▒░ ░▒▓█▓▒░
+# ░▒▓███████▓▒░░▒▓████████▓▒░▒▓█▓▒░░▒▓█▓▒░      ░▒▓███████▓▒░ ░▒▓██████▓▒░  ░▒▓█▓▒░
+#
+# ======================================================================================
+
+# Including this module globally enables static analysis integrations.
+
+include_guard (GLOBAL)
+
+option (STATIC_ANALYSIS "Enable static analysis integrations" ON)
+
+if (NOT STATIC_ANALYSIS)
+    return ()
+endif ()
+
+# cppcheck
+
+find_program (CPPCHECK_PROGRAM cppcheck DOC "cppcheck executable")
+
+if (CPPCHECK_PROGRAM)
+    set (cppcheck_build_dir "${CMAKE_BINARY_DIR}/cppcheck")
+
+    file (MAKE_DIRECTORY "${cppcheck_build_dir}")
+
+    set (
+        CMAKE_CXX_CPPCHECK
+        "${CPPCHECK_PROGRAM}"
+        "--cppcheck-build-dir=${cppcheck_build_dir}"
+        --inline-suppr
+        --std=c++20
+        --language=c++
+        --enable=warning
+        --enable=style
+        --enable=performance
+        --enable=portability
+        --suppress=missingIncludeSystem
+        --suppress=preprocessorErrorDirective
+        --suppress=unknownMacro
+        --suppress=unusedStructMember
+        --suppress=duplInheritedMember
+        --suppress=normalCheckLevelMaxBranches
+    )
+endif ()
