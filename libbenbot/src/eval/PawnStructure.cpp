831b9b80411b4472a96108b3944fdb1207d7459b
Ben Vining <ben.the.vining@gmail.com>
2025-09-17 18:00:45 -0500

    fix: warning

--- libbenbot/src/eval/PawnStructure.cpp
+++ libbenbot/src/eval/PawnStructure.cpp
@@ -43,7 +43,7 @@ namespace {
 
         static constexpr auto OtherSide = chess::pieces::other_side<Side>();
 
-        static constexpr auto promotionRank = Side == Color::White ? Rank::Eight : Rank::One;
+        static constexpr auto promotionRank = Side == Color::White ? Rank::Eight : Rank::One; // cppcheck-suppress knownConditionTrueFalse
 
         static constexpr auto ROOK_BEHIND_BONUS           = 25;
         static constexpr auto KING_ESCORT_BONUS           = 2;
