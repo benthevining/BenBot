f07cefcd4720693275286b39819dc6442b05f9d6
Ben Vining <ben.the.vining@gmail.com>
2025-09-18 02:20:59 -0500

    fix: Windows build of large page alloc functions

--- libchess/src/util/Memory_Windows.hpp
+++ libchess/src/util/Memory_Windows.hpp
@@ -20,98 +20,114 @@
 #    define WIN32_LEAN_AND_MEAN 1
 #endif
 
-#if _WIN32_WINNT < 0x0601
-#    undef _WIN32_WINNT
-#    define _WIN32_WINNT 0x0601 // Force to include needed API prototypes
+#ifdef _WIN32_WINNT
+#    if _WIN32_WINNT < 0x0601
+#        undef _WIN32_WINNT
+#    endif
 #endif
 
-#include <windows.h>
+#define _WIN32_WINNT 0x0601 // Force to include needed API prototypes
+
+#include <Windows.h>
 
 namespace chess::util {
 
 using std::size_t;
 
-[[nodiscard]] inline void* page_aligned_alloc_win_internal([[maybe_unused]] const size_t size)
-{
+namespace impl {
+    template <typename Func>
+    [[nodiscard]] Func find_function(HMODULE handle, LPCSTR name)
+    {
+        using VoidFuncPtr = void (*)();
+
+        return reinterpret_cast<Func>(
+            reinterpret_cast<VoidFuncPtr>(GetProcAddress(handle, name)));
+    }
+
+    [[nodiscard]] inline void* page_aligned_alloc_internal([[maybe_unused]] const size_t size)
+    {
 #ifndef _WIN64
-    return nullptr;
+        return nullptr;
 #else
-    using OpenProcessToken_t      = bool (*)(HANDLE, DWORD, PHANDLE);
-    using LookupPrivilegeValueA_t = bool (*)(LPCSTR, LPCSTR, PLUID);
-    using AdjustTokenPrivileges_t = bool (*)(HANDLE, BOOL, PTOKEN_PRIVILEGES, DWORD, PTOKEN_PRIVILEGES, PDWORD);
+        using OpenProcessToken_t      = bool (*)(HANDLE, DWORD, PHANDLE);
+        using LookupPrivilegeValueA_t = bool (*)(LPCSTR, LPCSTR, PLUID);
+        using AdjustTokenPrivileges_t = bool (*)(HANDLE, BOOL, PTOKEN_PRIVILEGES, DWORD, PTOKEN_PRIVILEGES, PDWORD);
 
-    const auto largePageSize = GetLargePageMinimum();
+        const auto largePageSize = GetLargePageMinimum();
 
-    if (largePageSize == 0uz)
-        return nullptr;
+        if (largePageSize == 0uz)
+            return nullptr;
 
-    auto hAdvapi32 = GetModuleHandle(TEXT("advapi32.dll"));
+        auto* hAdvapi32 = GetModuleHandle(TEXT("advapi32.dll"));
 
-    if (! hAdvapi32)
-        hAdvapi32 = LoadLibrary(TEXT("advapi32.dll"));
+        if (hAdvapi32 == nullptr)
+            hAdvapi32 = LoadLibrary(TEXT("advapi32.dll"));
 
-    auto OpenProcessToken_f = OpenProcessToken_t((void (*)())GetProcAddress(hAdvapi32, "OpenProcessToken"));
+        if (hAdvapi32 == nullptr)
+            return nullptr;
 
-    if (! OpenProcessToken_f)
-        return nullptr;
+        const auto OpenProcessToken_f = find_function<OpenProcessToken_t>(hAdvapi32, "OpenProcessToken");
 
-    auto LookupPrivilegeValueA_f = LookupPrivilegeValueA_t((void (*)())GetProcAddress(hAdvapi32, "LookupPrivilegeValueA"));
+        if (OpenProcessToken_f == nullptr)
+            return nullptr;
 
-    if (! LookupPrivilegeValueA_f)
-        return nullptr;
+        const auto LookupPrivilegeValueA_f = find_function<LookupPrivilegeValueA_t>(hAdvapi32, "LookupPrivilegeValueA");
 
-    auto AdjustTokenPrivileges_f = AdjustTokenPrivileges_t((void (*)())GetProcAddress(hAdvapi32, "AdjustTokenPrivileges"));
+        if (LookupPrivilegeValueA_f == nullptr)
+            return nullptr;
 
-    if (! AdjustTokenPrivileges_f)
-        return nullptr;
+        const auto AdjustTokenPrivileges_f = find_function<AdjustTokenPrivileges_t>(hAdvapi32, "AdjustTokenPrivileges");
 
-    // We need SeLockMemoryPrivilege, so try to enable it for the process
+        if (AdjustTokenPrivileges_f == nullptr)
+            return nullptr;
 
-    HANDLE hProcessToken {};
+        // We need SeLockMemoryPrivilege, so try to enable it for the process
 
-    if (! OpenProcessToken_f(
-            GetCurrentProcess(), TOKEN_ADJUST_PRIVILEGES | TOKEN_QUERY, &hProcessToken)) {
-        return nullptr;
-    }
+        HANDLE hProcessToken {};
 
-    LUID  luid {};
-    void* mem { nullptr };
-
-    if (LookupPrivilegeValueA_f(nullptr, "SeLockMemoryPrivilege", &luid)) {
-        TOKEN_PRIVILEGES tp {};
-        TOKEN_PRIVILEGES prevTp {};
-        DWORD            prevTpLen = 0;
-
-        tp.PrivilegeCount           = 1;
-        tp.Privileges[0].Luid       = luid;
-        tp.Privileges[0].Attributes = SE_PRIVILEGE_ENABLED;
-
-        // Try to enable SeLockMemoryPrivilege. Note that even if AdjustTokenPrivileges()
-        // succeeds, we still need to query GetLastError() to ensure that the privileges
-        // were actually obtained.
-
-        if (AdjustTokenPrivileges_f(hProcessToken, FALSE, &tp, sizeof(TOKEN_PRIVILEGES), &prevTp,
-                &prevTpLen)
-            && GetLastError() == ERROR_SUCCESS) {
-            // round up size to full pages and allocate
-            size = (size + largePageSize - 1uz) & ~(largePageSize - 1uz);
-
-            mem = VirtualAlloc(nullptr, size, MEM_RESERVE | MEM_COMMIT | MEM_LARGE_PAGES, PAGE_READWRITE);
-
-            // Privilege no longer needed, restore previous state
-            AdjustTokenPrivileges_f(hProcessToken, FALSE, &prevTp, 0, nullptr, nullptr);
+        if (! OpenProcessToken_f(
+                GetCurrentProcess(), TOKEN_ADJUST_PRIVILEGES | TOKEN_QUERY, &hProcessToken)) {
+            return nullptr;
         }
-    }
 
-    CloseHandle(hProcessToken);
+        LUID  luid {};
+        void* mem { nullptr };
 
-    return mem;
+        if (LookupPrivilegeValueA_f(nullptr, "SeLockMemoryPrivilege", &luid)) {
+            TOKEN_PRIVILEGES tp {}; // NOLINT(readability-identifier-length)
+            TOKEN_PRIVILEGES prevTp {};
+            DWORD            prevTpLen = 0;
+
+            tp.PrivilegeCount           = 1;
+            tp.Privileges[0].Luid       = luid;
+            tp.Privileges[0].Attributes = SE_PRIVILEGE_ENABLED;
+
+            // Try to enable SeLockMemoryPrivilege. Note that even if AdjustTokenPrivileges()
+            // succeeds, we still need to query GetLastError() to ensure that the privileges
+            // were actually obtained.
+
+            if (AdjustTokenPrivileges_f(hProcessToken, FALSE, &tp, sizeof(TOKEN_PRIVILEGES), &prevTp, &prevTpLen)
+                && GetLastError() == ERROR_SUCCESS) {
+                // round up size to full pages and allocate
+                const auto actualSize = (size + largePageSize - 1uz) & ~(largePageSize - 1uz);
+
+                mem = VirtualAlloc(nullptr, actualSize, MEM_RESERVE | MEM_COMMIT | MEM_LARGE_PAGES, PAGE_READWRITE);
+
+                // Privilege no longer needed, restore previous state
+                AdjustTokenPrivileges_f(hProcessToken, FALSE, &prevTp, 0, nullptr, nullptr);
+            }
+        }
+
+        CloseHandle(hProcessToken);
+
+        return mem;
 #endif
-}
+    }
+} // namespace impl
 
 [[nodiscard]] inline void* page_aligned_alloc_impl(const size_t size)
 {
-    if (auto* mem = page_aligned_alloc_win_internal(size)) // cppcheck-suppress knownConditionTrueFalse
+    if (auto* mem = impl::page_aligned_alloc_internal(size)) // cppcheck-suppress knownConditionTrueFalse
         return mem;
 
     return VirtualAlloc(nullptr, size, MEM_RESERVE | MEM_COMMIT, PAGE_READWRITE);
