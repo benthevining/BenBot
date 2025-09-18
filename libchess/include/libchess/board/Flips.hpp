e42b95008033522767bfbd8a8a723ee866ada369
Ben Vining <ben.the.vining@gmail.com>
2025-09-18 15:04:50 -0500

    feat: horizontal slip function (#291)

--- libchess/include/libchess/board/Flips.hpp
+++ libchess/include/libchess/board/Flips.hpp
@@ -32,6 +32,11 @@ namespace chess::board::flips {
  */
 [[nodiscard, gnu::const]] constexpr Bitboard vertical(Bitboard board) noexcept;
 
+/** Returns a copy of the bitboard, mirrored horizontally.
+    @ingroup board
+ */
+[[nodiscard, gnu::const]] constexpr Bitboard horizontal(Bitboard board) noexcept;
+
 /*
                          ___                           ,--,
       ,---,            ,--.'|_                ,--,   ,--.'|
@@ -54,6 +59,19 @@ constexpr Bitboard vertical(const Bitboard board) noexcept
     return Bitboard {
         std::byteswap(board.to_int())
     };
+}
+
+constexpr Bitboard horizontal(Bitboard board) noexcept
+{
+    static constexpr Bitboard k1 { 0x5555555555555555 };
+    static constexpr Bitboard k2 { 0x3333333333333333 };
+    static constexpr Bitboard k4 { 0x0f0f0f0f0f0f0f0f };
+
+    board = ((board >> 1uz) & k1) | ((board & k1) << 1uz);
+    board = ((board >> 2uz) & k2) | ((board & k2) << 2uz);
+    board = ((board >> 4uz) & k4) | ((board & k4) << 4uz);
+
+    return board;
 }
 
 } // namespace chess::board::flips
