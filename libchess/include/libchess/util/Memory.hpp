60dc0a7129262ad17dcbe52a034266bc7a62b9a4
Ben Vining <ben.the.vining@gmail.com>
2025-09-18 01:17:02 -0500

    feat: initial commit of page-aligned allocation functions

--- libchess/include/libchess/util/Memory.hpp
+++ libchess/include/libchess/util/Memory.hpp
@@ -12,49 +12,31 @@
  * ======================================================================================
  */
 
-#include <functional>
-#include <libchess/util/Threading.hpp>
-#include <utility>
+/** @file
+    This file provides some memory management utility functions.
+    @ingroup util
+ */
 
-#ifndef LIBCHESS_ARM
-#    if defined(__arm__) || defined(__arm64__)
-#        define LIBCHESS_ARM 1
-#    endif
-#endif
+#pragma once
 
-#ifndef LIBCHESS_INTEL
-#    if defined(i386) || defined(__i386__) || defined(__i386) || defined(_M_IX86) || defined(__x86_64__) || defined(_M_X64)
-#        define LIBCHESS_INTEL 1
-#    endif
-#endif
-
-#if defined(LIBCHESS_ARM) && defined(LIBCHESS_INTEL)
-#    error "Both ARM and Intel detected!"
-#endif
-
-#ifdef LIBCHESS_ARM
-#    include "ProgressiveBackoff_ARM.hpp"
-#elifdef LIBCHESS_INTEL
-#    include "ProgressiveBackoff_Intel.hpp"
-#else
-#    warning "Neither ARM nor Intel detected, using fallback implementation of progressive backoff"
-
-#    include <thread>
-
-namespace {
-void progressive_backoff_impl(std::function<bool()> pred)
-{
-    while (not pred())
-        std::this_thread::yield();
-}
-} // namespace
-#endif
+#include <cstddef> // IWYU pragma: keep - for size_t
 
 namespace chess::util {
 
-void progressive_backoff(std::function<bool()> pred)
-{
-    progressive_backoff_impl(std::move(pred));
-}
+/** Allocates memory aligned by page size, with a minimum
+    alignment of 4096 bytes. Memory allocated by this function
+    must be freed by calling ``page_aligned_free()``. Returns
+    ``nullptr`` if the memory could not be allocated.
+
+    @ingroup util
+ */
+[[nodiscard]] void* page_aligned_alloc(std::size_t size);
+
+/** Frees page-aligned memory allocated by ``page_aligned_alloc()``.
+    This is a no-op if ``mem`` is ``nullptr``.
+
+    @ingroup util
+ */
+void page_aligned_free(void* mem);
 
 } // namespace chess::util
--- libchess/src/util/Memory.cpp
+++ libchess/src/util/Memory.cpp
@@ -12,49 +12,17 @@
  * ======================================================================================
  */
 
-#include <functional>
-#include <libchess/util/Threading.hpp>
-#include <utility>
-
-#ifndef LIBCHESS_ARM
-#    if defined(__arm__) || defined(__arm64__)
-#        define LIBCHESS_ARM 1
-#    endif
-#endif
-
-#ifndef LIBCHESS_INTEL
-#    if defined(i386) || defined(__i386__) || defined(__i386) || defined(_M_IX86) || defined(__x86_64__) || defined(_M_X64)
-#        define LIBCHESS_INTEL 1
-#    endif
-#endif
-
-#if defined(LIBCHESS_ARM) && defined(LIBCHESS_INTEL)
-#    error "Both ARM and Intel detected!"
-#endif
-
-#ifdef LIBCHESS_ARM
-#    include "ProgressiveBackoff_ARM.hpp"
-#elifdef LIBCHESS_INTEL
-#    include "ProgressiveBackoff_Intel.hpp"
-#else
-#    warning "Neither ARM nor Intel detected, using fallback implementation of progressive backoff"
-
-#    include <thread>
-
-namespace {
-void progressive_backoff_impl(std::function<bool()> pred)
-{
-    while (not pred())
-        std::this_thread::yield();
-}
-} // namespace
-#endif
+#include <cstddef> // IWYU pragma: keep - for size_t
+#include <libchess/util/Memory.hpp>
 
 namespace chess::util {
 
-void progressive_backoff(std::function<bool()> pred)
+void* page_aligned_alloc(std::size_t size)
 {
-    progressive_backoff_impl(std::move(pred));
+}
+
+void page_aligned_free(void* mem)
+{
 }
 
 } // namespace chess::util
