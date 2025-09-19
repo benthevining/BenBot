f2f7b167b5220f971789754e8294a5ecbbfdf9fb
Ben Vining <ben.the.vining@gmail.com>
2025-09-18 23:28:45 -0500

    chore: removed unused memory utils

--- libchess/include/libchess/util/Memory.hpp
+++ libchess/include/libchess/util/Memory.hpp
@@ -20,10 +20,6 @@
 #pragma once
 
 #include <cstddef> // IWYU pragma: keep - for size_t
-#include <memory>
-#include <memory_resource>
-#include <stdexcept>
-#include <vector>
 
 namespace chess::util {
 
@@ -46,118 +42,5 @@ using std::size_t;
     @see PageAlignedMemoryResource, PageAlignedVector
  */
 void page_aligned_free(void* mem);
-
-/** This RAII struct encapsulates a memory buffer allocated using ``page_aligned_alloc()``
-    and handed to a ``std::pmr::monotonic_buffer_resource``, for usage with e.g.
-    ``std::pmr::vector``.
-
-    @ingroup util
-    @see PageAlignedVector
- */
-struct PageAlignedMemoryResource final {
-    /** The memory resource type owned by this object. */
-    using Resource = std::pmr::monotonic_buffer_resource;
-
-    /** Creates the memory resource and allocates its buffer. */
-    explicit PageAlignedMemoryResource(const size_t bufSizeBytes)
-        : buffer { page_aligned_alloc(bufSizeBytes) }
-        , resource { buffer, bufSizeBytes }
-    {
-    }
-
-    ~PageAlignedMemoryResource() { page_aligned_free(buffer); }
-
-    PageAlignedMemoryResource(const PageAlignedMemoryResource&)            = delete;
-    PageAlignedMemoryResource& operator=(const PageAlignedMemoryResource&) = delete;
-
-    PageAlignedMemoryResource(PageAlignedMemoryResource&&)            = delete;
-    PageAlignedMemoryResource& operator=(PageAlignedMemoryResource&&) = delete;
-
-    /** Returns the underlying memory resource. */
-    [[nodiscard]] const Resource& get_resource() const noexcept { return resource; }
-
-private:
-    void* buffer;
-
-    Resource resource;
-};
-
-/** This class encapsulates a ``std::vector`` that allocates its items using
-    a ``PageAlignedMemoryResource``. When the vector is resized, the old one
-    and its resource are simply destroyed.
-
-    @ingroup util
-    @see PageAlignedMemoryResource
- */
-template <typename T>
-class PageAlignedVector final {
-public:
-    /** Deletes all items in the vector and frees its memory. */
-    void clear() { vec.reset(); }
-
-    /** Deletes any items in the vector, frees its storage, and reserves
-        a new storage buffer large for at least ``maxSize`` objects.
-     */
-    void resize(const size_t maxSize)
-    {
-        clear();
-
-        vec = std::make_unique<InternalVec>(maxSize);
-    }
-
-    /** Returns the maximum number of elements the vector can hold. */
-    [[nodiscard]] size_t capacity() const noexcept
-    {
-        if (vec == nullptr)
-            return 0uz;
-
-        return vec->maxSize;
-    }
-
-    /** Returns a reference to the element in the vector at the given index.
-
-        @throws std::out_of_range An exception will be thrown if the vector
-        is empty or the index is out of range.
-     */
-    [[nodiscard]] T& at(const size_t idx)
-    {
-        if (vec == nullptr)
-            throw std::out_of_range { "PageAlignedVector::at() - internal vector doesn't exist!" };
-
-        return vec->at(idx);
-    }
-
-    /** Returns a reference to the element in the vector at the given index.
-
-        @throws std::out_of_range An exception will be thrown if the vector
-        is empty or the index is out of range.
-     */
-    [[nodiscard]] const T& at(const size_t idx) const
-    {
-        if (vec == nullptr)
-            throw std::out_of_range { "PageAlignedVector::at() - internal vector doesn't exist!" };
-
-        return vec->at(idx);
-    }
-
-private:
-    struct InternalVec final {
-        explicit InternalVec(const size_t maxNumItems)
-            : maxSize { maxNumItems }
-            , resource { maxNumItems * sizeof(T) }
-        {
-        }
-
-        size_t maxSize;
-
-        PageAlignedMemoryResource resource;
-
-        std::pmr::polymorphic_allocator<T> alloc { &resource.get_resource() };
-
-        std::pmr::vector<T> vector { alloc };
-    };
-
-    std::unique_ptr<InternalVec> vec;
-};
 
 } // namespace chess::util
