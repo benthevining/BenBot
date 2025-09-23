1654e47fa1ffea771ec01b223bf64129e0e5938e
Ben Vining <ben.the.vining@gmail.com>
2025-09-23 13:46:03 -0500

    refactor: searcher thread now has cpp file

--- libbenbot/src/search/Thread.cpp
+++ libbenbot/src/search/Thread.cpp
@@ -12,70 +12,70 @@
  * ======================================================================================
  */
 
-/** @file
-    This file defines the searcher thread class.
-    @ingroup search
- */
-
-#pragma once
-
-#include <atomic>
-#include <libbenbot/search/Callbacks.hpp>
-#include <libbenbot/search/Context.hpp>
-#include <libchess/game/Position.hpp>
-#include <thread>
-
-namespace chess::uci {
-struct GoCommandOptions;
-} // namespace chess::uci
+#include <libbenbot/search/Thread.hpp>
+#include <libchess/uci/CommandParsing.hpp>
+#include <libchess/util/Threading.hpp>
+#include <utility>
 
 namespace ben_bot::search {
 
-/** A worker thread that can be used to execute a search.
+Thread::Thread(Callbacks&& callbacksToUse)
+    : context { std::move(callbacksToUse) }
+{
+}
 
-    @ingroup search
- */
-struct Thread final {
-    /** Creates a searcher thread with a specified set of result callbacks.
-        Note that the result callbacks will be invoked on the background thread.
-     */
-    explicit Thread(Callbacks&& callbacksToUse);
+Thread::~Thread()
+{
+    threadShouldExit.store(true);
+    context.abort();
+    searcherThread.join();
+}
 
-    ~Thread();
+void Thread::set_position(const Position& pos)
+{
+    context.wait();
 
-    Thread(const Thread&)            = delete;
-    Thread& operator=(const Thread&) = delete;
-    Thread(Thread&&)                 = delete;
-    Thread& operator=(Thread&&)      = delete;
+    context.options.position = pos;
 
-    /** The context used for this thread's searches.
-        Note that the background thread will reference this object, so it should
-        only be modified after calling ``context.wait()``.
-     */
-    Context context;
+    // clear this so that all legal moves will be searched by default
+    context.options.movesToSearch.clear();
+}
 
-    /** Sets the position to be searched by the next search invocation.
-        This method blocks waiting for any previously executing search to complete.
-     */
-    void set_position(const Position& pos);
+void Thread::start(const chess::uci::GoCommandOptions& options)
+{
+    context.set_pondering(options.ponderMode);
 
-    /** Begins searching asynchronously.
-        This method returns immediately, and the actual search will be performed by
-        a background thread.
-     */
-    /// @{
-    void start(const chess::uci::GoCommandOptions& options);
+    context.wait(); // shouldn't have been searching, but better safe than sorry
 
-    void start();
-    /// @}
+    context.options.update_from(options);
 
-private:
-    void thread_func();
+    startSearch.store(true);
+}
 
-    std::thread searcherThread { [this] { thread_func(); } };
+void Thread::start()
+{
+    context.wait(); // shouldn't have been searching, but better safe than sorry
 
-    std::atomic_bool threadShouldExit { false };
-    std::atomic_bool startSearch { false };
-};
+    startSearch.store(true);
+}
+
+void Thread::thread_func()
+{
+    while (true) {
+        // we want to use progressive backoff to wait on the startSearch flag,
+        // but we also need to exit the PB loop if the threadShouldExit flag
+        // gets set
+        chess::util::progressive_backoff([this] {
+            return threadShouldExit.load() or startSearch.exchange(false);
+        });
+
+        if (threadShouldExit.load()) {
+            [[unlikely]];
+            return;
+        }
+
+        context.search();
+    }
+}
 
 } // namespace ben_bot::search
