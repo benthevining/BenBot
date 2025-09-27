d81234c14e3798c6afe8f5baf591b03aa5eb75bc
Ben Vining <ben.the.vining@gmail.com>
2025-09-26 22:32:18 -0500

    refactor: monadic funcs

--- libchess/src/notation/UCI.cpp
+++ libchess/src/notation/UCI.cpp
@@ -62,43 +62,42 @@ MoveOrError from_uci(
         return Move {};
     }
 
-    const auto from = Square::from_string(text.substr(0uz, 2uz));
-
-    if (not from.has_value())
-        return std::unexpected(from.error());
-
-    text = text.substr(2uz);
-
     return Square::from_string(text.substr(0uz, 2uz))
-        .and_then([&text, &position, start = from.value()](const Square dest) -> MoveOrError {
-            text                 = text.substr(2uz);
-            const auto movedType = position.our_pieces().get_piece_on(start);
+        .and_then([&text, &position](const Square from) {
+            text = text.substr(2uz);
 
-            if (not movedType.has_value()) {
-                [[unlikely]];
-                return std::unexpected(
-                    std::format(
-                        "No piece for color {} can move from square {}",
-                        magic_enum::enum_name(position.sideToMove), start));
-            }
+            return Square::from_string(text.substr(0uz, 2uz))
+                .and_then([&text, &position, from](const Square dest) -> MoveOrError {
+                    text = text.substr(2uz);
 
-            // promotion
-            if (not text.empty()) {
-                [[unlikely]];
+                    const auto movedType = position.our_pieces().get_piece_on(from);
 
-                return pieces::from_string(text)
-                    .transform([start, dest, type = movedType.value()](const pieces::Type promotedType) {
-                        return Move { start, dest, type, promotedType };
-                    })
-                    .or_else([](const std::string_view parseError) -> MoveOrError {
+                    if (not movedType.has_value()) {
+                        [[unlikely]];
                         return std::unexpected(
                             std::format(
-                                "Error parsing promoted type: {}",
-                                parseError));
-                    });
-            }
+                                "No piece for color {} can move from square {}",
+                                magic_enum::enum_name(position.sideToMove), from));
+                    }
 
-            return Move { start, dest, movedType.value() };
+                    // promotion
+                    if (not text.empty()) {
+                        [[unlikely]];
+
+                        return pieces::from_string(text)
+                            .transform([from, dest, type = movedType.value()](const pieces::Type promotedType) {
+                                return Move { from, dest, type, promotedType };
+                            })
+                            .or_else([](const std::string_view parseError) -> MoveOrError {
+                                return std::unexpected(
+                                    std::format(
+                                        "Error parsing promoted type: {}",
+                                        parseError));
+                            });
+                    }
+
+                    return Move { from, dest, movedType.value() };
+                });
         });
 }
 
