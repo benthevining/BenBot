5e20740a10b9253f26e1b1d5416361312e23f876
Ben Vining <ben.the.vining@gmail.com>
2025-09-27 21:00:25 -0500

    test: tests for split_at_first_space() (#315)

--- tests/unit/libchess/util/Strings.cpp
+++ tests/unit/libchess/util/Strings.cpp
@@ -34,3 +34,40 @@ TEST_CASE("Strings - trim()", TAGS)
     REQUIRE(trim(" 123456 ") == "123456");
     REQUIRE(trim("\n123456\n") == "123456");
 }
+
+TEST_CASE("Strings - split_at_first_space()", TAGS)
+{
+    using chess::util::split_at_first_space;
+
+    SECTION("Empty string")
+    {
+        const auto [before, after] = split_at_first_space({});
+
+        REQUIRE(before.empty());
+        REQUIRE(after.empty());
+    }
+
+    SECTION("No whitespace")
+    {
+        const auto [before, after] = split_at_first_space("abcdef");
+
+        REQUIRE(before == "abcdef");
+        REQUIRE(after.empty());
+    }
+
+    SECTION("Starts with whitespace")
+    {
+        const auto [before, after] = split_at_first_space(" abcdef");
+
+        REQUIRE(before.empty());
+        REQUIRE(after == "abcdef");
+    }
+
+    SECTION("Two words")
+    {
+        const auto [before, after] = split_at_first_space("123 456");
+
+        REQUIRE(before == "123");
+        REQUIRE(after == "456");
+    }
+}
