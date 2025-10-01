da9b92313e8b72616e94032e3e5b25379aa11a3f
Ben Vining <ben.the.vining@gmail.com>
2025-10-01 00:58:32 -0500

    test: initial e2e tests

--- tests/e2e/EndToEnd.py
+++ tests/e2e/EndToEnd.py
@@ -12,11 +12,18 @@
 import argparse
 import collections
 import concurrent.futures
+import io
 import subprocess
+import sys
+import time
+import traceback
+from contextlib import redirect_stdout
 from functools import wraps
 from pathlib import Path
 
 #
+
+MAX_TIMEOUT = 60 * 5
 
 
 class TimeoutException(Exception):
@@ -46,27 +53,23 @@ def timeout_decorator(timeout: float):
 
 
 class BenBot:
-    def __init__(self, path: str):
-        self.path = path
+    def __init__(self):
         self.process = None
         self.output = []
 
-        self.start()
-
-    def _check_process_alive(self):
-        if not self.process or self.process.poll() is not None:
-            print("\n".join(self.output))
-            raise RuntimeError("BenBot process has terminated")
-
-    def start(self):
         self.process = subprocess.Popen(
-            [self.path],
+            ENGINE_PATH,
             stdin=subprocess.PIPE,
             stdout=subprocess.PIPE,
             stderr=subprocess.STDOUT,
             universal_newlines=True,
             bufsize=1,
         )
+
+    def _check_process_alive(self):
+        if not self.process or self.process.poll() is not None:
+            print("\n".join(self.output))
+            raise RuntimeError("BenBot process has terminated")
 
     def send_command(self, command: str):
         if not self.process:
@@ -77,8 +80,39 @@ class BenBot:
         self.process.stdin.write(f"{command}\n")
         self.process.stdin.flush()
 
-    def setoption(self, name: str, value: str):
+    def set_option(self, name: str, value: str):
         self.send_command(f"setoption name {name} value {value}")
+
+    @timeout_decorator(MAX_TIMEOUT)
+    def output_contains(self, expected_output: str):
+        for line in self.readline():
+            if line == expected_output:
+                return
+
+    def readline(self):
+        if not self.process:
+            raise RuntimeError("BenBot process is not started")
+
+        while True:
+            self._check_process_alive()
+            line = self.process.stdout.readline().strip()
+            self.output.append(line)
+
+            yield line
+
+    def clear_output(self):
+        self.output = []
+
+    def quit(self):
+        self.send_command("quit")
+
+    def close(self) -> int:
+        if self.process:
+            self.process.stdin.close()
+            self.process.stdout.close()
+            return self.process.wait()
+
+        return 0
 
 
 class OrderedClassMembers(type):
@@ -91,6 +125,129 @@ class OrderedClassMembers(type):
             key for key in classdict.keys() if key not in ("__module__", "__qualname__")
         ]
         return type.__new__(cls, name, bases, classdict)
+
+
+class BenBotTests(metaclass=OrderedClassMembers):
+    def before_all(self):
+        self.engine = BenBot()
+
+    def after_all(self):
+        self.engine.quit()
+        assert self.engine.close() == 0
+
+    def after_each(self):
+        self.engine.clear_output()
+
+    def test_uci_command(self):
+        self.engine.send_command("uci")
+        self.engine.output_contains("uciok")
+
+
+class TestFramework:
+    def __init__(self):
+        self.passed_tests = 0
+        self.failed_tests = 0
+
+    def num_failed(self) -> int:
+        return self.failed_tests
+
+    def run(self, classes: list[type]):
+        self.start_time = time.time()
+
+        for test_class in classes:
+            try:
+                self.__run(test_class)
+            except Exception as e:
+                print(f"\nError: {e}")
+
+        self.__print_summary(round(time.time() - self.start_time, 2))
+
+    def __run(self, test_class: type) -> bool:
+        test_instance = test_class()
+        test_name = test_instance.__class__.__name__
+        test_methods = [m for m in test_instance.__ordered__ if m.startswith("test_")]
+
+        print(f"\nTest Suite: {test_name}")
+
+        if hasattr(test_instance, "before_all"):
+            test_instance.before_all()
+
+        fails = 0
+
+        for method in test_methods:
+            fails += self.__run_test_method(test_instance, method)
+
+        if hasattr(test_instance, "after_all"):
+            test_instance.after_all()
+
+        self.failed_tests += fails
+
+        return fails > 0
+
+    def __run_test_method(self, test_instance, method: str) -> int:
+        print(f"    Running {method}... \r", end="", flush=True)
+
+        buffer = io.StringIO()
+        fails = 0
+
+        try:
+            t0 = time.time()
+
+            with redirect_stdout(buffer):
+                if hasattr(test_instance, "before_each"):
+                    test_instance.before_each()
+
+                getattr(test_instance, method)()
+
+                if hasattr(test_instance, "after_each"):
+                    test_instance.after_each()
+
+            duration = time.time() - t0
+
+            self.print_success(f" {method} ({duration * 1000:.2f}ms)")
+            self.passed_tests += 1
+        except Exception as e:
+            if isinstance(e, TimeoutException):
+                self.print_failure(
+                    f" {method} (hit execution limit of {e.timeout} seconds)"
+                )
+
+            if isinstance(e, AssertionError):
+                self.__handle_assertion_error(t0, method)
+
+            fails += 1
+        finally:
+            self.__print_buffer_output(buffer)
+
+        return fails
+
+    def __handle_assertion_error(self, start_time, method: str):
+        duration = time.time() - start_time
+        self.print_failure(f" {method} ({duration * 1000:.2f}ms)")
+        traceback_output = "".join(traceback.format_tb(sys.exc_info()[2]))
+
+        print(traceback_output)
+
+    def __print_buffer_output(self, buffer: io.StringIO):
+        output = buffer.getvalue()
+        if output:
+            indented_output = "\n".join(f"    {line}" for line in output.splitlines())
+            print(f"    ⎯⎯⎯⎯⎯OUTPUT⎯⎯⎯⎯⎯")
+            print(f"{indented_output}")
+            print(f"    ⎯⎯⎯⎯⎯OUTPUT⎯⎯⎯⎯⎯")
+
+    def __print_summary(self, duration: float):
+        print(f"\nTest Summary\n")
+        print(
+            f"    Tests:       {self.passed_tests} passed, {self.failed_tests} failed, {self.passed_tests + self.failed_tests} total"
+        )
+        print(f"    Time:        {duration}s\n")
+
+    def print_failure(self, add: str):
+        print(f"    ✗{add}", flush=True)
+
+    def print_success(self, add: str):
+        print(f"    ✓{add}", flush=True)
 
 
 #
@@ -107,7 +264,8 @@ args = parser.parse_args()
 
 ENGINE_PATH = Path(args.engine).resolve()
 
-# test UCI response
+framework = TestFramework()
 
+framework.run([BenBotTests])
 
-exit(0)
+exit(framework.num_failed())
