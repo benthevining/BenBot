f18d5047f490f8fa3181a1e62c88c833f0a9d46d
Ben Vining <ben.the.vining@gmail.com>
2025-10-07 20:48:47 -0500

    test: evaluation symmetry tests (#362)

---
+++ tests/unit/libbenbot/eval/Evaluation.cpp
@@ -0,0 +1,72 @@
+/*
+ * ======================================================================================
+ *
+ * ░▒▓███████▓▒░░▒▓████████▓▒░▒▓███████▓▒░       ░▒▓███████▓▒░ ░▒▓██████▓▒░▒▓████████▓▒░
+ * ░▒▓█▓▒░░▒▓█▓▒░▒▓█▓▒░      ░▒▓█▓▒░░▒▓█▓▒░      ░▒▓█▓▒░░▒▓█▓▒░▒▓█▓▒░░▒▓█▓▒░ ░▒▓█▓▒░
+ * ░▒▓█▓▒░░▒▓█▓▒░▒▓█▓▒░      ░▒▓█▓▒░░▒▓█▓▒░      ░▒▓█▓▒░░▒▓█▓▒░▒▓█▓▒░░▒▓█▓▒░ ░▒▓█▓▒░
+ * ░▒▓███████▓▒░░▒▓██████▓▒░ ░▒▓█▓▒░░▒▓█▓▒░      ░▒▓███████▓▒░░▒▓█▓▒░░▒▓█▓▒░ ░▒▓█▓▒░
+ * ░▒▓█▓▒░░▒▓█▓▒░▒▓█▓▒░      ░▒▓█▓▒░░▒▓█▓▒░      ░▒▓█▓▒░░▒▓█▓▒░▒▓█▓▒░░▒▓█▓▒░ ░▒▓█▓▒░
+ * ░▒▓█▓▒░░▒▓█▓▒░▒▓█▓▒░      ░▒▓█▓▒░░▒▓█▓▒░      ░▒▓█▓▒░░▒▓█▓▒░▒▓█▓▒░░▒▓█▓▒░ ░▒▓█▓▒░
+ * ░▒▓███████▓▒░░▒▓████████▓▒░▒▓█▓▒░░▒▓█▓▒░      ░▒▓███████▓▒░ ░▒▓██████▓▒░  ░▒▓█▓▒░
+ *
+ * ======================================================================================
+ */
+
+#include <catch2/catch_test_macros.hpp>
+#include <libbenbot/eval/Evaluation.hpp>
+#include <libchess/game/Position.hpp>
+#include <libchess/notation/FEN.hpp>
+
+static constexpr auto TAGS { "[eval][evaluation]" };
+
+using ben_bot::eval::evaluate;
+using chess::game::Position;
+using chess::notation::from_fen;
+
+TEST_CASE("Evaluation symmetry", TAGS)
+{
+    SECTION("Starting position")
+    {
+        static const Position startPos;
+
+        REQUIRE(
+            evaluate(startPos)
+            == evaluate(flipped(startPos)));
+    }
+
+    SECTION("Kiwipete")
+    {
+        const auto position = from_fen("r3k2r/p1ppqpb1/bn2pnp1/3PN3/1p2P3/2N2Q1p/PPPBBPPP/R3K2R w KQkq - 0 1").value();
+
+        REQUIRE(
+            evaluate(position)
+            == evaluate(flipped(position)));
+    }
+
+    SECTION("Mate in 1")
+    {
+        const auto position = from_fen("r2qkb1r/ppp1p1pp/2n3p1/1b6/2BpP3/5Q2/PPPP1PPP/RNB1K1NR w KQkq - 0 1").value();
+
+        REQUIRE(
+            evaluate(position)
+            == evaluate(flipped(position)));
+    }
+
+    SECTION("Mate")
+    {
+        const auto position = from_fen("8/8/8/8/8/8/3q4/2K1k3 w - - 0 1").value();
+
+        REQUIRE(
+            evaluate(position)
+            == evaluate(flipped(position)));
+    }
+
+    SECTION("Promoting")
+    {
+        const auto position = from_fen("8/2P5/8/8/k1K5/8/8/8 w - - 0 1").value();
+
+        REQUIRE(
+            evaluate(position)
+            == evaluate(flipped(position)));
+    }
+}
