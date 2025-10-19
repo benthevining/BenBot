5211bad15a4994e1d6415bd326526e04188ed8a3
Ben Vining <ben.the.vining@gmail.com>
2025-10-19 17:04:03 -0500

    refactor: progressive_backoff() function is now implemented in a single source file

--- libchess/src/util/Progressivebackoff.cpp
+++ libchess/src/util/Progressivebackoff.cpp
@@ -12,15 +12,18 @@
  * ======================================================================================
  */
 
-#pragma once
-
-#include <arm_acle.h>
+#include "util/DetectArch.hpp" // IWYU pragma: keep
 #include <functional>
+#include <libchess/util/Threading.hpp>
 #include <thread>
+
+#ifdef LIBCHESS_ARM
+
+#    include <arm_acle.h>
 
 namespace chess::util {
 
-inline void progressive_backoff_impl(std::function<bool()> pred)
+void progressive_backoff(std::function<bool()> pred)
 {
     // approx. 2x10 ns (= 20 ns) and 750x1333 ns (~ 1 ms), respectively,
     // on an Apple Silicon Mac or an armv8 based phone
@@ -46,3 +49,69 @@ inline void progressive_backoff_impl(std::function<bool()> pred)
 }
 
 } // namespace chess::util
+
+#elifdef LIBCHESS_INTEL
+
+#    include <emmintrin.h>
+
+namespace chess::util {
+
+void progressive_backoff(std::function<bool()> pred)
+{
+    // approx. 5x5 ns (= 25 ns), 10x40 ns (= 400 ns), and 3000x350 ns (~ 1 ms),
+    // respectively, when measured on a 2.9 GHz Intel i9
+    static constexpr auto N0 = 5uz;    // NOLINT(readability-identifier-length)
+    static constexpr auto N1 = 10uz;   // NOLINT(readability-identifier-length)
+    static constexpr auto N2 = 3000uz; // NOLINT(readability-identifier-length)
+
+    for (auto i = 0uz; i < N0; ++i) {
+        if (pred())
+            return;
+    }
+
+    for (auto i = 0uz; i < N1; ++i) {
+        if (pred())
+            return;
+
+        _mm_pause();
+    }
+
+    while (true) {
+        for (auto i = 0uz; i < N2; ++i) {
+            if (pred())
+                return;
+
+            // do not roll these into a loop: not every compiler unrolls it
+            _mm_pause();
+            _mm_pause();
+            _mm_pause();
+            _mm_pause();
+            _mm_pause();
+            _mm_pause();
+            _mm_pause();
+            _mm_pause();
+            _mm_pause();
+            _mm_pause();
+        }
+
+        // waiting longer than we should, let's give other threads a chance to recover
+        std::this_thread::yield();
+    }
+}
+
+} // namespace chess::util
+
+#else
+#    warning "Neither ARM nor Intel detected, using fallback implementation of progressive backoff"
+
+namespace chess::util {
+
+void progressive_backoff(std::function<bool()> pred)
+{
+    while (not pred())
+        std::this_thread::yield();
+}
+
+} // namespace chess::util
+
+#endif
