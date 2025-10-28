d7d41e17bae573f494bc7e6d65fd8edf5d016edc
Ben Vining <ben.the.vining@gmail.com>
2025-10-28 02:46:43 -0500

    test: test case to verify TextTable::to_string() and TextTable::print()

---
+++ tests/unit/libchess/util/TextTable.cpp
@@ -0,0 +1,64 @@
+/*
+ * ======================================================================================
+ *
+ * ░▒▓███████▓▒░░▒▓████████▓▒░▒▓███████▓▒░       ░▒▓███████▓▒░ ░▒▓██████▓▒░▒▓████████▓▒░
+ * ░▒▓█▓▒░░▒▓█▓▒░▒▓█▓▒░      ░▒▓█▓▒░░▒▓█▓▒░      ░▒▓█▓▒░░▒▓█▓▒░▒▓█▓▒░░▒▓█▓▒░ ░▒▓█▓▒░
+ * ░▒▓█▓▒░░▒▓█▓▒░▒▓█▓▒░      ░▒▓█▓▒░░▒▓█▓▒░      ░▒▓█▓▒░░▒▓█▓▒░▒▓█▓▒░░▒▓█▓▒░ ░▒▓█▓▒░
+ * ░▒▓███████▓▒░░▒▓██████▓▒░ ░▒▓█▓▒░░▒▓█▓▒░      ░▒▓███████▓▒░░▒▓█▓▒░░▒▓█▓▒░ ░▒▓█▓▒░
+ * ░▒▓█▓▒░░▒▓█▓▒░▒▓█▓▒░      ░▒▓█▓▒░░▒▓█▓▒░      ░▒▓█▓▒░░▒▓█▓▒░▒▓█▓▒░░▒▓█▓▒░ ░▒▓█▓▒░
+ * ░▒▓█▓▒░░▒▓█▓▒░▒▓█▓▒░      ░▒▓█▓▒░░▒▓█▓▒░      ░▒▓█▓▒░░▒▓█▓▒░▒▓█▓▒░░▒▓█▓▒░ ░▒▓█▓▒░
+ * ░▒▓███████▓▒░░▒▓████████▓▒░▒▓█▓▒░░▒▓█▓▒░      ░▒▓███████▓▒░ ░▒▓██████▓▒░  ░▒▓█▓▒░
+ *
+ * ======================================================================================
+ */
+
+#include <catch2/catch_test_macros.hpp>
+#include <libchess/util/TextTable.hpp>
+#include <string>
+#include <string_view>
+
+static constexpr auto TAGS { "[util][strings][TextTable]" };
+
+using chess::util::strings::TextTable;
+
+namespace {
+[[nodiscard]] auto get_printed_string(const TextTable& table) -> std::string
+{
+    std::string result;
+
+    auto appendToString = [&result](const std::string_view toAdd) {
+        result.append(toAdd);
+    };
+
+    table.print(
+        appendToString, appendToString, appendToString,
+        [&result] { result.append(1uz, '\n'); });
+
+    return result;
+}
+} // namespace
+
+TEST_CASE("TextTable - to_string() should give same result as print()", TAGS)
+{
+    TextTable table;
+
+    table.append_column("Option")
+        .append_column("Type")
+        .append_column("Notes")
+        .append_column("Default");
+
+    table.new_row()
+        .append_column("Option 1")
+        .append_column("Integer")
+        .append_column("Comments")
+        .append_column("0");
+
+    table.new_row()
+        .append_column("Option 2")
+        .append_column("String")
+        .append_column("More comments")
+        .append_column("foo");
+
+    REQUIRE(
+        get_printed_string(table) == table.to_string());
+}
