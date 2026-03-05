496129507c76e78c1e300fd78ae25bdcb9a6e4d5
Ben Vining <ben.the.vining@gmail.com>
2026-03-05 03:17:36 -0600

    fix: only setting gcov as CTEST_COVERAGE_COMMAND if compiler is gcc
    No functional change.
    Bench: 5586985

--- config/cmake/Coverage.cmake
+++ config/cmake/Coverage.cmake
@@ -90,20 +90,19 @@ if (CMAKE_C_COMPILER_ID MATCHES "Clang")
     )
 endif ()
 
-# add script & custom target to clean old coverage output
+# add custom target to clean old coverage output
 
-set (configured_script "${CMAKE_BINARY_DIR}/DeleteOldCoverageOutput.cmake")
-
-set (COVERAGE_OUTPUT_DIR "${CMAKE_BINARY_DIR}/coverage")
+set (COVERAGE_OUTPUT_DIR "${CMAKE_BINARY_DIR}/coverage/$<CONFIG>")
 set (COVERAGE_INFO_FILE "${COVERAGE_OUTPUT_DIR}/coverage.info")
 
-configure_file (
-    "${CMAKE_CURRENT_LIST_DIR}/detail/DeleteOldCoverageOutput.cmake" "${configured_script}" @ONLY
-)
-
 add_custom_target (
-    coverage-clean COMMAND "${CMAKE_COMMAND}" -P "${configured_script}"
-    COMMENT "Cleaning old coverage output files..." VERBATIM
+    coverage-clean
+    COMMAND
+        "${CMAKE_COMMAND}" -D "COVERAGE_OUTPUT_DIR=${COVERAGE_OUTPUT_DIR}" -D
+        "CMAKE_BINARY_DIR=${CMAKE_BINARY_DIR}" -D "CMAKE_SOURCE_DIR=${CMAKE_SOURCE_DIR}" -P
+        "${CMAKE_CURRENT_LIST_DIR}/detail/DeleteOldCoverageOutput.cmake"
+    COMMENT "[coverage] - Cleaning old coverage output files..."
+    VERBATIM
 )
 
 # set up coverage reports
@@ -130,19 +129,16 @@ if (NOT (GENINFO_PROGRAM AND GENHTML_PROGRAM))
     return ()
 endif ()
 
-# cmake-format: off
+include (FetchContent)
+
 add_custom_command (
     OUTPUT "${COVERAGE_INFO_FILE}"
     COMMAND
-        "${GENINFO_PROGRAM}" "${CMAKE_SOURCE_DIR}"
-        --exclude /_deps/
-        --gcov-tool "${GCOV_PROGRAM}"
-        --branch-coverage --no-external --follow
-        --forget-test-names --demangle-cpp --keep-going
+        "${GENINFO_PROGRAM}" "${CMAKE_SOURCE_DIR}" --exclude "${FETCHCONTENT_BASE_DIR}" --gcov-tool
+        "${GCOV_PROGRAM}" --branch-coverage --no-external --follow --forget-test-names
+        --demangle-cpp --keep-going --rc derive_function_end_line=0 -o "${COVERAGE_INFO_FILE}"
         --ignore-errors empty,inconsistent,format,unsupported,category,range,source,unused
-        --rc derive_function_end_line=0
-        -o "${COVERAGE_INFO_FILE}"
-    COMMENT "Running geninfo on .gcda coverage output files..."
+    COMMENT "[coverage] - Running geninfo on .gcda coverage output files..."
     VERBATIM
 )
 
@@ -151,25 +147,20 @@ set (index_html "${COVERAGE_OUTPUT_DIR}/index.html")
 add_custom_command (
     OUTPUT "${index_html}"
     COMMAND
-        "${GENHTML_PROGRAM}" "${COVERAGE_INFO_FILE}"
-        --header-title "${CMAKE_PROJECT_NAME} coverage report"
-        --prefix "${CMAKE_SOURCE_DIR}"
-        --precision 1
-        --filter "brace,blank,range,function"
-        --show-zero-columns --suppress-aliases
-        --simplified-colors --elide-path-mismatch --sort --dark-mode
-        --synthesize-missing --show-navigation --legend --function-coverage
-        --branch-coverage --demangle-cpp --forget-test-names --keep-going
-        --ignore-errors empty,inconsistent,format,unsupported,category,range,source
-        -o "${COVERAGE_OUTPUT_DIR}"
+        "${GENHTML_PROGRAM}" "${COVERAGE_INFO_FILE}" --header-title
+        "${CMAKE_PROJECT_NAME} coverage report" --prefix "${CMAKE_SOURCE_DIR}" --precision 1
+        --filter "brace,blank,range,function" --show-zero-columns --suppress-aliases
+        --simplified-colors --elide-path-mismatch --sort --dark-mode --synthesize-missing
+        --show-navigation --legend --function-coverage --branch-coverage --demangle-cpp
+        --forget-test-names --keep-going -o "${COVERAGE_OUTPUT_DIR}" --ignore-errors
+        empty,inconsistent,format,unsupported,category,range,source
     COMMAND "${CMAKE_COMMAND}" -E echo
-        "Coverage report generated. Open ${index_html} in your browser."
+            "Coverage report generated. Open ${index_html} in your browser."
     WORKING_DIRECTORY "${CMAKE_SOURCE_DIR}"
-    MAIN_DEPENDENCY "${COVERAGE_INFO_FILE}"
-    COMMENT "Running genhtml to create coverage report..."
+    DEPENDS "${COVERAGE_INFO_FILE}"
+    COMMENT "[coverage] - Running genhtml to create coverage report..."
     VERBATIM
 )
-# cmake-format: on
 
 add_custom_target (coverage-report DEPENDS "${index_html}")
 
--- config/cmake/detail/DeleteOldCoverageOutput.cmake
+++ config/cmake/detail/DeleteOldCoverageOutput.cmake
@@ -10,15 +10,13 @@
 #
 # ======================================================================================
 
-# This file was configured by CMake! Any changes will be overwritten!
+cmake_minimum_required (VERSION 4.0.0 FATAL_ERROR)
 
-cmake_minimum_required (VERSION "@CMAKE_VERSION@" FATAL_ERROR)
-
-file (REMOVE_RECURSE "@COVERAGE_OUTPUT_DIR@")
+file (REMOVE_RECURSE "${COVERAGE_OUTPUT_DIR}")
 
 file (GLOB_RECURSE coverage_files LIST_DIRECTORIES false
-      "@CMAKE_BINARY_DIR@/*.gcda" "@CMAKE_BINARY_DIR@/*.gcov" "@CMAKE_BINARY_DIR@/*.profraw"
-      "@CMAKE_SOURCE_DIR@/*.gcda" "@CMAKE_SOURCE_DIR@/*.gcov" "@CMAKE_SOURCE_DIR@/*.profraw"
+      "${CMAKE_BINARY_DIR}/*.gcda" "${CMAKE_BINARY_DIR}/*.gcov" "${CMAKE_BINARY_DIR}/*.profraw"
+      "${CMAKE_SOURCE_DIR}/*.gcda" "${CMAKE_SOURCE_DIR}/*.gcov" "${CMAKE_SOURCE_DIR}/*.profraw"
 )
 
 if (coverage_files)
