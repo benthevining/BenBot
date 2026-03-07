a87fdf0ab7bc7c36c9c2d8c8a3f0e88790a2bdc5
Ben Vining <ben.the.vining@gmail.com>
2026-03-07 15:06:49 -0600

    feat: initial commit of json flag for perft command

--- libbenbot/src/engine/Perft.cpp
+++ libbenbot/src/engine/Perft.cpp
@@ -19,6 +19,7 @@
 #include <libchess/moves/Perft.hpp>
 #include <libchess/uci/Printing.hpp>
 #include <libchess/util/Strings.hpp>
+#include <nlohmann/json.hpp>
 #include <print>
 #include <string>
 
@@ -31,9 +32,12 @@ using uci::printing::info_string;
 namespace {
     using chess::moves::PerftResult;
 
+    // TODO: use a concept to match lambda type
+    using MovePrinter = std::function<std::string(Move)>;
+
     void print_root_nodes(
-        const PerftResult&                      result,
-        const std::function<std::string(Move)>& printMove)
+        const PerftResult& result,
+        const MovePrinter& printMove)
     {
         for (const auto& [move, numChildren] : result.rootNodes) {
             info_string(std::format(
@@ -42,7 +46,8 @@ namespace {
         }
     }
 
-    void print_results(const PerftResult& result)
+    void print_results(
+        const PerftResult& result)
     {
         info_string(std::format("Nodes: {}", result.nodes));
         info_string(std::format("Captures: {}", result.captures));
@@ -55,27 +60,56 @@ namespace {
         // NB. the python wrapper script relies on this being printed last
         info_string(std::format("Stalemates: {}", result.stalemates));
     }
+
+    void pretty_print(
+        const PerftResult& result,
+        const MovePrinter& printMove)
+    {
+        std::println("");
+        print_root_nodes(result, printMove);
+
+        std::println("");
+        print_results(result);
+    }
+
+    void print_json(
+        const PerftResult& result)
+    {
+        nlohmann::json json;
+
+        json["captures"]    = result.captures;
+        json["castles"]     = result.castles;
+        json["checkmates"]  = result.checkmates;
+        json["checks"]      = result.checks;
+        json["en_passants"] = result.enPassantCaptures;
+        json["promotions"]  = result.promotions;
+        json["stalemates"]  = result.stalemates;
+        json["totalNodes"]  = result.nodes;
+
+        std::println("{}", json.dump());
+    }
 } // namespace
 
-void Engine::run_perft(const string_view arguments) const
+void Engine::run_perft(
+    const string_view arguments) const
 {
-    const auto depth = util::strings::int_from_string(
-        util::strings::trim(arguments),
-        4uz);
+    using util::strings::trim;
+
+    const auto [depthStr, jsonFlag] = util::strings::split_at_first_space(arguments);
+
+    const auto depth = util::strings::int_from_string(trim(depthStr), 4uz);
 
     info_string(std::format("Running perft depth {}...", depth));
 
     const auto result = chess::moves::perft(
         depth, searcher.context.options.position);
 
-    std::println("");
-
-    print_root_nodes(
-        result,
-        [this](const Move move) { return pretty_print_move(move); });
-
-    std::println("");
-    print_results(result);
+    if (trim(jsonFlag) == "json") {
+        print_json(result);
+    } else {
+        pretty_print(result,
+            [this](const Move move) { return pretty_print_move(move); });
+    }
 }
 
 } // namespace ben_bot
