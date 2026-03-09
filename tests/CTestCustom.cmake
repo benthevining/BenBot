ac3b2e89f5e25367fef6a5967762b32e9a5d8cda
Ben Vining <ben.the.vining@gmail.com>
2026-03-08 21:43:54 -0500

    fix: warnings

--- tests/CTestCustom.cmake
+++ tests/CTestCustom.cmake
@@ -23,19 +23,25 @@ set (CTEST_CUSTOM_MAXIMUM_PASSED_TEST_OUTPUT_SIZE 1000000) # 1 MB
 
 set (CTEST_CUSTOM_MAXIMUM_NUMBER_OF_WARNINGS 100)
 
-list (APPEND CTEST_CUSTOM_COVERAGE_EXCLUDE @CMAKE_PREFIX_PATH@ "@FETCHCONTENT_BASE_DIR@" "_deps")
-
 # this matches forward- or back-slashes
 set (slash "[/\\]")
+
+list (APPEND CTEST_CUSTOM_COVERAGE_EXCLUDE @CMAKE_PREFIX_PATH@ "@FETCHCONTENT_BASE_DIR@"
+      "${slash}_deps${slash}"
+)
 
 list (
     APPEND
     CTEST_CUSTOM_WARNING_EXCEPTION
     @CMAKE_PREFIX_PATH@
     "@FETCHCONTENT_BASE_DIR@"
-    "_deps"
+    "${slash}_deps${slash}"
     "bits/sigaction.h"
     "Program Files\\\\Microsoft Visual Studio"
     "libbenbot${slash}resources${slash}__cmrc_ben_bot_resources_internal${slash}"
     "libchess${slash}src${slash}util${slash}memory${slash}PageAlignedAlloc_Windows.hpp"
+    # MSVC warnings about the Visitor class's move operators being implicitly deleted
+    "chess::util::Visitor<"
+    # warnings about std::getenv() being deprecated/thread-unsafe
+    "main.cpp.+getenv"
 )
