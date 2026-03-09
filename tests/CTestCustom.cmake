5c01e673f834de33ff2bda97bfbc78ac13d30862
Ben Vining <ben.the.vining@gmail.com>
2026-03-08 23:29:46 -0500

    fix: warnings

--- tests/CTestCustom.cmake
+++ tests/CTestCustom.cmake
@@ -39,7 +39,7 @@ list (
     "bits/sigaction.h"
     "Program Files\\\\Microsoft Visual Studio"
     "libbenbot${slash}resources${slash}__cmrc_ben_bot_resources_internal${slash}"
-    "libutil${slash}src${slash}util${slash}memory${slash}PageAlignedAlloc_Windows.hpp"
+    "libutil${slash}src${slash}memory${slash}PageAlignedAlloc_Windows.hpp"
     # MSVC warnings about the Visitor class's move operators being implicitly deleted
     "chess::util::Visitor<"
     # warnings about std::getenv() being deprecated/thread-unsafe
