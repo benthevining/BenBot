e73f068cdd5fd81b11ede180a6d41e14beb505f8
Ben Vining <ben.the.vining@gmail.com>
2026-03-16 03:26:28 -0500

    feat: initial work on board_editor() function

--- gui/libgui/src/BoardEditor.cpp
+++ gui/libgui/src/BoardEditor.cpp
@@ -12,61 +12,227 @@
  * ======================================================================================
  */
 
+#include <beman/inplace_vector/inplace_vector.hpp>
 #include <format>
 #include <imgui.h>
+#include <imgui_stdlib.h>
 #include <libchess/board/File.hpp>
 #include <libchess/board/Rank.hpp>
 #include <libchess/board/Square.hpp>
 #include <libchess/game/Position.hpp>
+#include <libchess/moves/Move.hpp>
+#include <libchess/moves/MoveGen.hpp>
+#include <libchess/notation/FEN.hpp>
+#include <libchess/pieces/Colors.hpp>
 #include <libgui/BoardEditor.hpp>
 #include <magic_enum/magic_enum.hpp>
+#include <optional>
 #include <ranges>
+#include <string>
+#include <string_view>
 
 namespace ben_bot::gui {
 
 using chess::board::File;
 using chess::board::Rank;
 using chess::board::Square;
+using chess::pieces::Color;
+
+// render piece sprites in squares
+// sprite "trays" to click & drag pieces on to board
+// ability to click & drag pieces on board to different squares
+// ability to click & drag pieces on board off of board (or to a trash can icon?)
+
+namespace {
+    void render_chessboard(Position& position)
+    {
+        static constexpr auto colorWhite = IM_COL32(255, 255, 255, 255);
+        static constexpr auto colorBlack = IM_COL32(0, 0, 0, 255);
+
+        static constexpr auto squareSize = 50.f;
+
+        if (ImGui::BeginTable("Board", 8, ImGuiTableFlags_SizingFixedSame)) {
+            for (const auto file : magic_enum::enum_values<File>()) {
+                ImGui::TableSetupColumn(
+                    std::format("{}", file).c_str(),
+                    ImGuiTableColumnFlags_WidthFixed, squareSize);
+            }
+
+            for (const auto rank : magic_enum::enum_values<Rank>() | std::views::reverse) {
+                ImGui::TableNextRow(ImGuiTableRowFlags_None, squareSize);
+
+                for (const auto file : magic_enum::enum_values<File>()) {
+                    ImGui::TableNextColumn();
+
+                    const Square square { .file = file, .rank = rank };
+
+                    ImGui::TableSetBgColor(
+                        ImGuiTableBgTarget_CellBg,
+                        square.is_light() ? colorWhite : colorBlack);
+
+                    ImGui::Text("%s", std::format("{}", square).c_str());
+                }
+            }
+
+            ImGui::EndTable(); // Must be called to finish the table
+        }
+    }
+
+    void render_side_to_move(Position& position)
+    {
+        bool whiteToMove { position.sideToMove == Color::White };
+
+        if (ImGui::Checkbox("White to move", &whiteToMove)) {
+            if (whiteToMove)
+                position.sideToMove = Color::White;
+            else
+                position.sideToMove = Color::Black;
+        }
+
+        ImGui::SetItemTooltip("Set the side to move");
+    }
+
+    void render_castling_rights(Position& position)
+    {
+        ImGui::BeginGroup();
+
+        ImGui::Text("Castling Rights");
+
+        if (ImGui::BeginTable("CastlingRights", 3, ImGuiTableFlags_RowBg | ImGuiTableFlags_Borders)) {
+            ImGui::TableNextRow();
+
+            ImGui::TableNextColumn();
+            ImGui::Text("White");
+
+            ImGui::TableNextColumn();
+            ImGui::Checkbox("O-O##1", &position.whiteCastlingRights.kingside);
+
+            ImGui::TableNextColumn();
+            ImGui::Checkbox("O-O-O##1", &position.whiteCastlingRights.queenside);
+
+            ImGui::TableNextRow();
+
+            ImGui::TableNextColumn();
+            ImGui::Text("Black");
+
+            ImGui::TableNextColumn();
+            ImGui::Checkbox("O-O##2", &position.blackCastlingRights.kingside);
+
+            ImGui::TableNextColumn();
+            ImGui::Checkbox("O-O-O##2", &position.blackCastlingRights.queenside);
+
+            ImGui::EndTable();
+        }
+
+        ImGui::EndGroup();
+
+        if (ImGui::IsItemHovered())
+            ImGui::SetTooltip("Set the castling rights of each side");
+    }
+
+    [[nodiscard]] auto get_possible_ep_squares(const Position& position)
+        -> beman::inplace_vector::inplace_vector<Square, 2uz>
+    {
+        using chess::moves::detail::get_en_passant;
+
+        return (position.is_white_to_move()
+                       ? get_en_passant<Color::White>(position)
+                       : get_en_passant<Color::Black>(position))
+             | std::views::transform(&chess::moves::Move::to)
+             | std::ranges::to<beman::inplace_vector::inplace_vector<Square, 2uz>>();
+    }
+
+    void render_ep_square(Position& position)
+    {
+        const auto currEP = position.enPassantTargetSquare
+                                .transform([](const Square square) { return std::format("{}", square); })
+                                .value_or(std::string { });
+
+        static std::optional<Square> selectedSquare;
+
+        if (ImGui::BeginCombo("EPSquare", currEP.c_str(), ImGuiComboFlags_PopupAlignLeft)) {
+            const auto epSquares = get_possible_ep_squares(position);
+
+            if (not epSquares.empty()) {
+                for (const auto square : epSquares) {
+                    const bool isSelected = selectedSquare.has_value() and square == *selectedSquare;
+
+                    if (ImGui::Selectable(std::format("{}", square).c_str(), isSelected)) {
+                        selectedSquare                 = square;
+                        position.enPassantTargetSquare = selectedSquare;
+                    }
+
+                    if (isSelected)
+                        ImGui::SetItemDefaultFocus();
+                }
+
+                // "none" option
+                const bool isSelected = not selectedSquare.has_value();
+
+                if (ImGui::Selectable("None", isSelected)) {
+                    selectedSquare.reset();
+                    position.enPassantTargetSquare.reset();
+                }
+
+                if (isSelected)
+                    ImGui::SetItemDefaultFocus();
+            }
+
+            ImGui::EndCombo();
+        }
+    }
+
+    void render_fen_string(Position& position)
+    {
+        auto inputBuf = chess::notation::to_fen(position);
+
+        if (ImGui::InputText("FEN", &inputBuf, ImGuiInputTextFlags_AutoSelectAll)) {
+            [[maybe_unused]] const auto result
+                = chess::notation::from_fen(inputBuf)
+                      .transform([&position](const Position& newPos) {
+                          position = newPos;
+                          return std::monostate { };
+                      })
+                      .transform_error([](const std::string_view errorMessage) {
+                          // TODO: display error popup
+                          return std::monostate { };
+                      });
+        }
+
+        ImGui::SetItemTooltip("Enter a FEN string");
+    }
+} // namespace
 
 void board_editor(Position& position)
 {
-    static constexpr auto colorWhite = IM_COL32(255, 255, 255, 255);
-    static constexpr auto colorBlack = IM_COL32(0, 0, 0, 255);
+    if (ImGui::Begin("Board editor")) {
+        render_chessboard(position);
 
-    ImGui::Begin("Board editor");
+        render_side_to_move(position);
 
-    const auto [width, height] = ImGui::GetWindowSize();
+        render_castling_rights(position);
 
-    const auto squareSize = std::min(
-        width / 8.f, height / 8.f);
+        render_ep_square(position);
 
-    if (ImGui::BeginTable("Board", 8, ImGuiTableFlags_Borders | ImGuiTableFlags_Resizable)) {
-        for (const auto file : magic_enum::enum_values<File>()) {
-            ImGui::TableSetupColumn(
-                std::format("{}", file).data(),
-                ImGuiTableColumnFlags_WidthFixed, squareSize);
-        }
+        ImGui::BeginGroup();
 
-        for (const auto rank : magic_enum::enum_values<Rank>() | std::views::reverse) {
-            ImGui::TableNextRow(ImGuiTableRowFlags_None, squareSize);
+        if (ImGui::Button("Reset"))
+            position = Position { };
+        ImGui::SetItemTooltip("Reset the board to the starting position");
 
-            for (const auto file : magic_enum::enum_values<File>()) {
-                ImGui::TableNextColumn();
+        ImGui::SameLine();
+        if (ImGui::Button("Flip"))
+            position = flipped(position);
+        ImGui::SetItemTooltip("Flip the board vertically");
 
-                const Square square {
-                    .file = file,
-                    .rank = rank
-                };
+        ImGui::SameLine();
+        if (ImGui::Button("Clear"))
+            position = Position::empty();
+        ImGui::SetItemTooltip("Remove all pieces from the board");
 
-                ImGui::TableSetBgColor(
-                    ImGuiTableBgTarget_CellBg,
-                    square.is_light() ? colorWhite : colorBlack);
+        ImGui::EndGroup();
 
-                ImGui::Text("%s", std::format("{}", square).data());
-            }
-        }
-
-        ImGui::EndTable(); // Must be called to finish the table
+        render_fen_string(position);
     }
 
     ImGui::End();
