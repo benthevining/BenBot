611a65779e669bba124a98d34ea04cbb66f27ac4
Ben Vining <ben.the.vining@gmail.com>
2026-03-21 07:23:30 -0500

    chore: removed some comments

--- gui/main_glfw_vulkan.cpp
+++ gui/main_glfw_vulkan.cpp
@@ -424,11 +424,6 @@ int main(
     ImVec4 clear_color { 0.45f, 0.55f, 0.60f, 1.00f };
 
     while (not glfwWindowShouldClose(window)) {
-        // Poll and handle events (inputs, window resize, etc.)
-        // You can read the io.WantCaptureMouse, io.WantCaptureKeyboard flags to tell if dear imgui wants to use your inputs.
-        // - When io.WantCaptureMouse is true, do not dispatch mouse input data to your main application, or clear/overwrite your copy of the mouse data.
-        // - When io.WantCaptureKeyboard is true, do not dispatch keyboard input data to your main application, or clear/overwrite your copy of the keyboard data.
-        // Generally you may always pass all inputs to dear imgui, and hide them from your application based on those two flags.
         glfwPollEvents();
 
         // Resize swap chain?
