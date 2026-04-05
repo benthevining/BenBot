8192cc45b24138e90d5a23d8681519d750ade384
Ben Vining <ben.the.vining@gmail.com>
2026-04-04 22:04:53 -0500

    feat: game viewer has load & save buttons (#538)

--- gui/libgui/src/GameViewer.cpp
+++ gui/libgui/src/GameViewer.cpp
@@ -12,22 +12,115 @@
  * ======================================================================================
  */
 
+#include <array>
+#include <concepts>
+#include <filesystem>
 #include <imgui.h>
 #include <libchess/notation/PGN.hpp>
 #include <libgui/GameViewer.hpp>
 #include <libutil/Console.hpp>
+#include <libutil/Files.hpp>
+#include <nfd.hpp>
 #include <nlohmann/json.hpp>
 #include <string>
 #include <string_view>
 
 namespace ben_bot::gui {
 
+using chess::notation::GameRecord;
+using std::filesystem::path;
 using std::string_view;
+
+namespace {
+    template <typename Func>
+    concept FileCallback = std::regular_invocable<Func, const path&>;
+
+    template <bool IsLoading>
+    void show_file_dialog(FileCallback auto callback)
+    {
+        NFD::UniquePath outPath;
+
+        const auto result = [&outPath]() noexcept {
+            static constexpr std::array filters {
+                nfdu8filteritem_t { "PGN", "pgn" }
+            };
+
+            if constexpr (IsLoading) {
+                return OpenDialog(
+                    outPath,
+                    filters.data(), filters.size());
+            } else {
+                return SaveDialog(
+                    outPath,
+                    filters.data(), filters.size(),
+                    nullptr, "game.pgn");
+            }
+        }();
+
+        switch (result) {
+            case NFD_OKAY:
+                callback(path { outPath.get() });
+                break;
+
+            case NFD_CANCEL:
+                std::println("Info: user canceled file selection dialog");
+                break;
+
+            case NFD_ERROR:
+                std::println(
+                    stderr, "Info: error with file selection dialog");
+                break;
+
+            default: break;
+        }
+    }
+
+    void show_pgn_load_dialog(GameRecord& game)
+    {
+        show_file_dialog<true>(
+            [&game](const path& file) {
+                [[maybe_unused]] const auto result
+                    = util::files::load(file)
+                          .and_then([](const string_view text) {
+                              return chess::notation::from_pgn(text)
+                                  .transform_error([](const string_view error) { return std::string { error }; });
+                          })
+                          .transform([&game](const GameRecord& loaded) {
+                              game = loaded;
+                              return std::monostate { };
+                          })
+                          .transform_error(util::print_error);
+            });
+    }
+
+    void show_pgn_save_dialog(const GameRecord& game)
+    {
+        show_file_dialog<false>(
+            [&game](const path& file) {
+                [[maybe_unused]] const auto result
+                    = util::files::overwrite(file, to_pgn(game))
+                          .transform_error(util::print_error);
+            });
+    }
+} // namespace
 
 void render_game_viewer(
     GameViewerState& state, const bool showTooltips)
 {
     if (ImGui::Begin("Game")) {
+        if (ImGui::Button("Load"))
+            show_pgn_load_dialog(state.game);
+
+        if (showTooltips)
+            ImGui::SetItemTooltip("Load a game from a PGN file");
+
+        ImGui::SameLine();
+
+        if (ImGui::Button("Save"))
+            show_pgn_save_dialog(state.game);
+
+        if (showTooltips)
+            ImGui::SetItemTooltip("Save game to a PGN file");
     }
 
     ImGui::End();
@@ -55,7 +148,7 @@ auto GameViewerState::from_string(const string_view str) -> GameViewerState
     [[maybe_unused]] const auto result
         = chess::notation::from_pgn(
             parsed.at(TAG_PGN).get<string_view>())
-              .transform([&state](const chess::notation::GameRecord& loaded) {
+              .transform([&state](const GameRecord& loaded) {
                   state.game = loaded;
                   return std::monostate { };
               })
