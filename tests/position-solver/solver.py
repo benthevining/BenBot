af7bccafd15dfc158ce9e5651782709f8ec13b82
Ben Vining <ben.the.vining@gmail.com>
2026-04-08 17:44:54 -0500

    refactor: position solver script

--- tests/position-solver/solver.py
+++ tests/position-solver/solver.py
@@ -12,14 +12,53 @@
 import argparse
 import logging
 from pathlib import Path
-from typing import Tuple
 
 import chess.engine
 
-#
+
+def run_position_solver_test(
+    data_file: Path, engine_path: Path, log_file: Path, index: int
+) -> None:
+    logging.basicConfig(level=logging.DEBUG)
+
+    with open(data_file) as file:
+        epd_data = file.readlines()[index]
+
+    board = chess.Board()
+
+    operations = board.set_epd(epd_data)
+
+    print(f"Position ID: {operations['id']}")
+    print(f"FEN: {board.fen()}", flush=True)
+
+    engine = chess.engine.SimpleEngine.popen_uci(str(engine_path), timeout=None)
+
+    engine.configure({"Debug Log File": str(log_file)})
+
+    result = engine.play(board, chess.engine.Limit(depth=operations["depth"]))
+
+    engine.quit()
+
+    exit_code = engine.returncode.result()
+
+    if exit_code != 0:
+        print(f"Engine exited with code {exit_code}", flush=True)
+
+    expected_move = operations["bm"][0]
+
+    if result.move == expected_move:
+        print("Passed!")
+        exit(0)
+
+    print("FAILED!")
+    print(f"Expected {board.san(expected_move)}, got {board.san(result.move)}")
+
+    print(board.unicode(borders=True, empty_square=" "))
+
+    exit(1)
 
 
-def parse_args() -> Tuple[Path, Path, Path, int]:
+def parse_args() -> argparse.Namespace:
     parser = argparse.ArgumentParser(
         prog="PositionSolver",
         description="Run BenBot test positions",
@@ -27,71 +66,44 @@ def parse_args() -> Tuple[Path, Path, Path, int]:
     )
 
     parser.add_argument(
-        "-e", "--engine", required=True, help="Path to engine executable"
+        "--engine",
+        required=True,
+        type=lambda p: Path(p).resolve(),
+        help="Path to engine executable",
     )
-    parser.add_argument("-l", "--log", required=True, help="Path to engine log file")
+
     parser.add_argument(
-        "-t",
+        "--log",
+        required=True,
+        type=lambda p: Path(p).resolve(),
+        help="Path to engine log file",
+    )
+
+    parser.add_argument(
         "--test",
         required=True,
+        type=lambda p: Path(p).resolve(),
         help="Path to testcase data file (1 EPD per line)",
     )
+
     parser.add_argument(
-        "-i",
         "--index",
         type=int,
         required=True,
         help="Index of line to take from the testcase file",
     )
 
-    parsed = parser.parse_args()
-
-    return (
-        Path(parsed.test).resolve(),
-        Path(parsed.engine).resolve(),
-        Path(parsed.log).resolve(),
-        parsed.index,
-    )
+    return parser.parse_args()
 
 
 #
 
-logging.basicConfig(level=logging.DEBUG)
+if __name__ == "__main__":
+    args = parse_args()
 
-TESTCASE_FILE, ENGINE_PATH, ENGINE_LOG_PATH, index = parse_args()
-
-with open(TESTCASE_FILE) as file:
-    epd_data = file.readlines()[index]
-
-board = chess.Board()
-
-operations = board.set_epd(epd_data)
-
-print(f"Position ID: {operations['id']}")
-print(f"FEN: {board.fen()}", flush=True)
-
-engine = chess.engine.SimpleEngine.popen_uci(str(ENGINE_PATH), timeout=None)
-
-engine.configure({"Debug Log File": str(ENGINE_LOG_PATH.resolve())})
-
-result = engine.play(board, chess.engine.Limit(depth=operations["depth"]))
-
-engine.quit()
-
-exit_code = engine.returncode.result()
-
-if exit_code != 0:
-    print(f"Engine exited with code {exit_code}", flush=True)
-
-expectedMove = operations["bm"][0]
-
-if result.move == expectedMove:
-    print("Passed!")
-    exit(0)
-
-print("FAILED!")
-print(f"Expected {board.san(expectedMove)}, got {board.san(result.move)}")
-
-print(board.unicode(borders=True, empty_square=" "))
-
-exit(1)
+    run_position_solver_test(
+        data_file=args.test,
+        engine_path=args.engine,
+        index=args.index,
+        log_file=args.log,
+    )
